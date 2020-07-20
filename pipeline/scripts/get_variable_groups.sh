1445999fee119a8790585bcc9d548239b70b8f13
karanotts <nottinghamkara@gmail.com>
2020-07-20 12:25:37 +0100

    add script to read vargroups

---
+++ pipeline/scripts/get_variable_groups.sh
@@ -0,0 +1,21 @@
+
+#!/usr/bin/env bash
+
+set -e
+
+if [ $# -lt 2 ]
+  then
+    echo "Not enough arguments supplied, pass UserName and Azure Personal Access token"
+    exit 1
+fi
+ACCESSTOKEN=$1
+USERNAME=$2
+
+groupList=$(curl -X GET -s -u ${USERNAME}:${ACCESSTOKEN} "https://dev.azure.com/hmcts/Shared%20Services/_apis/distributedtask/variablegroups/?api-version=5.0-preview.1" | jq -r '.value[] | .name  | if contains("HMI") then . else empty end' )
+
+
+for group in ${groupList} ; do
+
+curl -X GET -s -u ${USERNAME}:${ACCESSTOKEN} "https://dev.azure.com/hmcts/Shared%20Services/_apis/distributedtask/variablegroups/?groupName=${group}&api-version=5.0-preview.1" | jq -r '.value[0] | del(.createdOn, .modifiedBy , .createdBy, .modifiedOn)' > ${group}.json
+echo "Updated ${group}"
+done
