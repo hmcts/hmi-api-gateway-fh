562c7a413188b4c39278066c43d18d201f13089f
karanotts <nottinghamkara@gmail.com>
2020-07-21 10:58:36 +0100

    adding storage for tests results

---
+++ infrastructure/terraform/04-inputs-storage.tf
@@ -0,0 +1,6 @@
+variable "storageaccount" {
+    description = "Storage account that will hold all data associated with APIM"
+}
+variable "storagevolume" {
+    description = "Share Volume to attach to ACI containers running tests"
+}
