bfc31d7c76ef1107d3a9839d353b033884a1ffdf
karanotts <nottinghamkara@gmail.com>
2020-07-21 07:43:08 +0100

    WiP - get user ad at creation of APIM

--- infrastructure/terraform/25-api-management-subscription.tf
+++ infrastructure/terraform/25-api-management-subscription.tf
@@ -1,9 +1,12 @@
-data "azurerm_api_management_user" "hmi_apim_user" {
+resource "azurerm_api_management_user" "hmi_apim_user" {
   api_management_name = azurerm_api_management.hmi_apim.name
   resource_group_name = azurerm_api_management.hmi_apim.resource_group_name
-  user_id             = "temp user"
+  user_id             = "5931a75ae4bbd512288c680b"
+  first_name          = "Test"
+  last_name           = "User"
+  email               = "test@hmcts.dev.null"
+  state               = "active"
 }
-
 resource "azurerm_api_management_subscription" "hmi_apim_subscription" {
   api_management_name = azurerm_api_management.hmi_apim.name
   resource_group_name = azurerm_api_management.hmi_apim.resource_group_name
