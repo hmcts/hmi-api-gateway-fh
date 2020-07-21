e8888eefc7566238f3ec832b98015b7f64ce8b03
463952 <subroto.mukherjee@HMCTS.NET>
2020-07-21 17:24:40 +0100

    HMIS Health Check: Adding a new get end point

--- infrastructure/terraform/26-api-management-api-operation-policy.tf
+++ infrastructure/terraform/26-api-management-api-operation-policy.tf
@@ -1,7 +1,7 @@
-resource "azurerm_api_management_api_operation_policy" "example" {
+resource "azurerm_api_management_api_operation_policy" "health_check_policy" {
   api_name            = azurerm_api_management_api.hmi_apim_api.name
   api_management_name = azurerm_api_management.hmi_apim.name
   resource_group_name = azurerm_resource_group.hmi_apim_rg.name
-  operation_id        = "request-hearing"
+  operation_id        = "health-check"
   xml_content = file("../template/api-request-hearings-policy.xml")
 }
