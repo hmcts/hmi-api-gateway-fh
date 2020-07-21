4db6a79ae823e51756560391cd8bafff6faa7a63
karanotts <nottinghamkara@gmail.com>
2020-07-21 10:54:11 +0100

    adding storage for tests results

--- infrastructure/terraform/12-storage-account.tf
+++ infrastructure/terraform/12-storage-account.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "hmi_apim_storage" {
-  name                     = "${var.prefix}${var.product}${var.environment}sa"
+  name                     = var.storageaccount
   location                 = var.location
   resource_group_name      = azurerm_resource_group.hmi_apim_rg.name
   account_tier             = "Standard"
@@ -14,7 +14,7 @@ resource "azurerm_storage_container" "hmi_apim_container" {
 }
 
 resource "azurerm_storage_share" "hmi_apim_fileshare" {
-  name                 = "acishare"
-  storage_account_name = azurerm_storage_account.example.name
+  name                 = var.storagevolume
+  storage_account_name = azurerm_storage_account.hmi_apim_storage.name
   quota                = 1
 }
