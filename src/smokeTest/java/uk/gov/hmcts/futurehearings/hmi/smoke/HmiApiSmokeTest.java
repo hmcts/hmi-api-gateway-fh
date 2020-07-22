1d7cd6270cda8152e507e27fd854e21c5478e8e4
SachinNaik <sachin.naik@hmcts.net>
2020-07-22 13:24:19 +0100

    Adding smoke test framework

---
+++ src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/HmiApiSmokeTest.java
@@ -0,0 +1,52 @@
+package uk.gov.hmcts.futurehearings.hmi.smoke;
+
+import static io.restassured.RestAssured.expect;
+
+import uk.gov.hmcts.futurehearings.hmi.functional.Application;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = {Application.class})
+@ActiveProfiles("smoke")
+public class HmiApiSmokeTest {
+
+    @Value("${targetInstance}")
+    private String targetInstance;
+
+    @Value("${targetHost}")
+    private String targetHost;
+
+    @Value("${targetSubscriptionKey}")
+    private String targetSubscriptionKey;
+
+    @Value("${hmiApiRootContext}")
+    private String hmiApiRootContext;
+
+    private Map<String, Object> headersAsMap = new HashMap<>();
+
+    @BeforeEach
+    public void initialiseValues() {
+        headersAsMap.put("Host", targetHost);
+        headersAsMap.put("Ocp-Apim-Subscription-Key", targetSubscriptionKey);
+        headersAsMap.put("Ocp-Apim-Trace", "true");
+        headersAsMap.put("Company-Name", "HMCTS");
+        headersAsMap.put("Content-Type", "application/json");
+    }
+
+    @Test
+    public void testSuccessfulHmiApiGet() {
+        expect().that().statusCode(200)
+                .given().contentType("application/json")
+                .headers(headersAsMap)
+                .baseUri(targetInstance)
+                .basePath(hmiApiRootContext)
+                .when().get();
+    }
+}
