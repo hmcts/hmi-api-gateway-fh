6e1576eac787f6cb50ac05ee4a2805badddda715
karanotts <nottinghamkara@gmail.com>
2020-07-22 14:08:03 +0100

    storage ctd

--- infrastructure/terraform/26-api-management-api-op-policy.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_api_management_api_operation_policy" "request_hearing_policy" {
-  api_name            = azurerm_api_management_api.hmi_apim_api.name
-  api_management_name = azurerm_api_management.hmi_apim.name
-  resource_group_name = azurerm_resource_group.hmi_apim_rg.name
-  operation_id        = "request-hearing"
-
-  xml_content = file("../template/api-op-req-hearing-policy.xml")
-}
