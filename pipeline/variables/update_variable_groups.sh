b0ebcd4d54340d5e8f4ffe9f35848163c410f057
karanotts <nottinghamkara@gmail.com>
2020-07-23 14:36:38 +0100

    comment out script

--- pipeline/variables/update_variable_groups.sh
+++ pipeline/variables/update_variable_groups.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-if [ $# -lt 2 ]
-  then
-    echo "Please provide username and PAT"
-    exit 1
-fi
+# if [ $# -lt 2 ]
+#   then
+#     echo "Please provide username and PAT"
+#     exit 1
+# fi
 
 for fileName in *.json; do
 
