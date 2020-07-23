b29e2df91d1e2e33ff9dbb01ea8899c95e4548f7
SachinNaik <sachin.naik@hmcts.net>
2020-07-23 17:44:50 +0100

    Fixing functional test

--- src/functionalTest/java/uk/gov/hmcts/futurehearings/hmi/functional/hearing/steps/HearingSteps.java
+++ src/functionalTest/java/uk/gov/hmcts/futurehearings/hmi/functional/hearing/steps/HearingSteps.java
@@ -1,10 +1,8 @@
 package uk.gov.hmcts.futurehearings.hmi.functional.hearing.steps;
 
-import static net.serenitybdd.rest.SerenityRest.lastResponse;
-import static uk.gov.hmcts.futurehearings.hmi.functional.hearing.steps.verify.HearingResponseVerification.verifySessionResponse;
-import static uk.gov.hmcts.futurehearings.hmi.functional.hearing.steps.verify.HearingResponseVerification.verifyHearingResponse;
 import static net.serenitybdd.rest.SerenityRest.expect;
-import static org.junit.Assert.assertEquals;
+import static net.serenitybdd.rest.SerenityRest.lastResponse;
+import static uk.gov.hmcts.futurehearings.hmi.functional.hearing.steps.verify.HearingResponseVerification.verifyHearingResponse;
 
 import java.util.Map;
 
@@ -14,21 +12,6 @@ import net.thucydides.core.annotations.Step;
 public class HearingSteps {
 
     private String actor;
-
-    @Step("#actor routes to {0} in order to get invoke {1}")
-    public void invokeEmployee (final String basePath,
-                                final String api,
-                                final Map<String,Object> headersAsMap) {
-
-        System.out.println("The value of the base path " + basePath);
-        Response response = expect().that().statusCode(200)
-                .given().contentType("application/json")
-                .headers(headersAsMap)
-                .baseUri(basePath)
-                .basePath(api)
-                .when().get().then().extract().response();
-        verifySessionResponse(lastResponse());
-    }
 
     @Step("#actor routes to {0} in order to get invoke {1}")
     public void requestHearing (final String api,
