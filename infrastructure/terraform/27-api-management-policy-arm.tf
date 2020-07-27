015ac0fa5b2b68a6c22d4718c3dabf26d7adfa5b
karanotts <nottinghamkara@gmail.com>
2020-07-27 16:17:24 +0100

    adding arm template policy

---
+++ infrastructure/terraform/27-api-management-policy-arm.tf
@@ -0,0 +1,41 @@
+resource "azurerm_template_deployment" "apim-policy" {
+    name                = "apim-policy-01"
+    resource_group_name = azurerm_resource_group.hmi_apim_rg.name
+    depends_on          = azurerm_api_management_product.hmi_apim_product
+    deployment_mode 	= "Incremental"
+    parameters          = {
+        "ApimServiceName" = azurerm_api_management.hmi_apim.name
+    	}
+
+
+    template_body 	    = <<DEPLOY
+{
+    "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {
+        "ApimServiceName": {
+            "type": "string",
+            "metadata": {
+                "description": "The name of the API Management"
+            }
+        },
+    "variables": {
+
+    }, 
+    "resources": [{
+            "type": "Microsoft.ApiManagement/service/products/policies",
+            "name": "[concat(parameters('ApimServiceName'), '/hmi-apim-product/policy')]",
+            "apiVersion": "2018-01-01",
+            "properties": {
+                "policyContent": "[../template/product-unlimited.policy.xml')]",
+                "contentFormat": "rawxml-link"
+            },
+            "targetResourceId": [
+                "[resourceId('Microsoft.ApiManagement/service/products', parameters('ApimServiceName'), 'hmi-apim-product')]"
+            ]
+        }],
+    "outputs": {}
+    }
+}
+DEPLOY
+}
