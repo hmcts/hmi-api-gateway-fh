2eeeaeb73c3fe4bfda938bc50b0fc8a7c2c8fdfa
karanotts <nottinghamkara@gmail.com>
2020-07-29 15:10:42 +0100

    still needs tags- keeping placeholder

--- infrastructure/terraform/01-inputs-shared.tf
+++ infrastructure/terraform/01-inputs-shared.tf
@@ -2,4 +2,11 @@ variable "prefix" {}
 variable "product" {}
 variable "environment" {}
 variable "location" {}
-variable "tags" {}
+variable "tags" {
+  description = "The tags to associate with your resources."
+  default = {
+    createdby = "HMI"
+    managedby = "HMI"
+    application = "Azure APIM Management"
+  }
+}
