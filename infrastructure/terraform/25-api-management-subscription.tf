8fa88e0b5cde990a49089b1a20e31b73a95d660e
karanotts <nottinghamkara@gmail.com>
2020-07-29 08:48:44 +0100

    mv kv

--- infrastructure/terraform/25-api-management-subscription.tf
+++ infrastructure/terraform/25-api-management-subscription.tf
@@ -20,6 +20,6 @@ resource "azurerm_api_management_subscription" "hmi_apim_subscription" {
 resource "azurerm_key_vault_secret" "subscription_key" {
   name         = "${var.prefix}-${var.product}-svc-${var.environment}-sub-key"
   value        = azurerm_api_management_subscription.hmi_apim_subscription.primary_key
-  key_vault_id = azurerm_key_vault.hmi_apim_kv.id
+  key_vault_id = data.azurerm_key_vault.devops_keyvault.id
   tags         = var.tags
 }
