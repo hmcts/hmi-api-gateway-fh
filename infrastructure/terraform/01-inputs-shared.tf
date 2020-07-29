7532a6c47eba56155004421029d5fc06c6b21422
karanotts <nottinghamkara@gmail.com>
2020-07-29 15:07:37 +0100

    readded params

--- infrastructure/terraform/01-inputs-shared.tf
+++ infrastructure/terraform/01-inputs-shared.tf
@@ -1,13 +1,5 @@
 variable "prefix" {}
 variable "product" {}
 variable "environment" {}
-variable "location" {
-  default = "uksouth"
-}
-variable "tags" {
-  description = "The tags to associate with your resources."
-  default = {
-    createdby = "HMI"
-    purpose   = "PoC"
-  }
-}
+variable "location" {}
+variable "tags" {}
