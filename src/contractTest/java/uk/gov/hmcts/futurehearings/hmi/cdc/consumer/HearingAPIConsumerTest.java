e318f9ef2ed0464e846d4a8b016660376a5071fa
SachinNaik <sachin.naik@hmcts.net>
2020-07-30 17:46:42 +0100

    fixing issues

--- src/contractTest/java/uk/gov/hmcts/futurehearings/hmi/cdc/consumer/HearingAPIConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/futurehearings/hmi/cdc/consumer/HearingAPIConsumerTest.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.futurehearings.hmi.cdc.consumer;
 
 import static uk.gov.hmcts.futurehearings.hmi.cdc.consumer.common.TestingUtils.readFileContents;
 
+import uk.gov.hmcts.futurehearings.hmi.functional.Application;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,32 +15,48 @@ import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.model.RequestResponsePact;
 import net.serenitybdd.rest.SerenityRest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ActiveProfiles("contract")
+@SpringBootTest(classes = {Application.class})
 @ExtendWith(PactConsumerTestExt.class)
 @ExtendWith(SpringExtension.class)
 public class HearingAPIConsumerTest {
 
+    @Value("${targetHost}")
+    private String targetHost;
+
+    @Value("${targetSubscriptionKey}")
+    private String targetSubscriptionKey;
+
     private static final String PROVIDER_REQUEST_HEARING_API = "/hmi/hearings";
 
-    @Pact(provider = "SandL_HEARING_API", consumer = "HMI_HEARING_API")
+    private Map<String, String> headersAsMap = new HashMap<>();
+
+    @BeforeEach
+    public void initialiseValues() {
+        headersAsMap.put("Host", targetHost);
+        headersAsMap.put("Ocp-Apim-Subscription-Key", targetSubscriptionKey);
+        headersAsMap.put("Ocp-Apim-Trace", "true");
+        headersAsMap.put("Company-Name", "HMCTS");
+        headersAsMap.put("Content-Type", "application/json");
+    }
+
+   @Pact(provider = "SandL_HEARING_API", consumer = "HMI_HEARING_API")
     public RequestResponsePact createRequestHearingAPIPact(
             PactDslWithProvider builder) throws IOException {
 
         String requestHearingPayload =
                 readFileContents("payloads/request-hearing.json");
-
-        Map<String, String> headersAsMap = new HashMap<>();
-        headersAsMap.put("Host", "hmi-apim-svc-test.azure-api.net");
-        headersAsMap.put("Ocp-Apim-Subscription-Key", "587c56f59a604054b1ce73dac0ea5b28");
-        headersAsMap.put("Ocp-Apim-Trace", "true");
-        headersAsMap.put("Company-Name", "HMCTS");
-        headersAsMap.put("Content-Type", "application/json");
 
         return builder
                 .given("Request Hearing API")
@@ -57,13 +75,6 @@ public class HearingAPIConsumerTest {
     public void shouldExecutePostHearingAndReturn200(MockServer mockServer) throws IOException {
         String requestHearingPayload =
                 readFileContents("payloads/request-hearing.json");
-
-        Map<String, String> headersAsMap = new HashMap<>();
-        headersAsMap.put("Host", "hmi-apim-svc-test.azure-api.net");
-        headersAsMap.put("Ocp-Apim-Subscription-Key", "587c56f59a604054b1ce73dac0ea5b28");
-        headersAsMap.put("Ocp-Apim-Trace", "true");
-        headersAsMap.put("Company-Name", "HMCTS");
-        headersAsMap.put("Content-Type", "application/json");
 
         SerenityRest
                 .given()
