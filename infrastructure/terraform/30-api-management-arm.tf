f8984319f9ff6049401ce46da0c3f4c464c49b32
karanotts <nottinghamkara@gmail.com>
2020-07-30 09:13:26 +0100

    changing policy deployment to ARM

--- infrastructure/terraform/30-api-management-arm.tf
+++ infrastructure/terraform/30-api-management-arm.tf
@@ -1,52 +1,56 @@
-# resource "azurerm_template_deployment" "apim-policy" {
-#     name                = "apim-policy-01"
-#     resource_group_name = azurerm_resource_group.hmi_apim_rg.name
-#     depends_on          = [azurerm_api_management_product.hmi_apim_product]
-#     deployment_mode 	= "Incremental"
-#     parameters          = {
-#         "ApimServiceName" = azurerm_api_management.hmi_apim.name
-#         "ApiName"         = azurerm_api_management_api.hmi_apim_api.name 
-#         "OperationId"     = "request-hearing"
-#     	}
+resource "azurerm_template_deployment" "apim-policy" {
+    name                = "apim-policy-01"
+    resource_group_name = azurerm_resource_group.hmi_apim_rg.name
+    depends_on          = [azurerm_api_management_product.hmi_apim_product]
+    deployment_mode 	= "Incremental"
+    parameters          = {
+        "apimServiceName" = azurerm_api_management.hmi_apim.name
+        "apiName"         = azurerm_api_management_api.hmi_apim_api.name 
+        "operationId"     = "request-hearing"
+        "method"          = "POST"
+        "format"          = "rawxml-link"
+        "urlTemplate"     = "/hearing"
+        "templateLink"    = "https://raw.githubusercontent.com/hmcts/hmi-api-gateway-fh/HMIS-152_SANDBOX_CI/CD_Pipeline-temp/infrastructure/template/api-op-req-hearing-policy.xml"
+    	}
 
 
-#     template_body 	    = <<DEPLOY
-# {
-#     "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
-#     "contentVersion": "1.0.0.0",
-#     "parameters": {
-#         "ApiName": {
-#             "type": "String"
-#         },
-#         "ApimServiceName": {
-#             "type": "String"
-#         },
-#         "OperationId": {
-#             "type": "String"
-#         }
-#     },
-#     "resources": [
-#         {
-#             "type": "Microsoft.ApiManagement/service/apis/operations",
-#             "apiVersion": "2019-12-01",
-#             "name": "[concat(parameters('ApimServiceName'), '/', parameters('ApiName'), '/', parameters('OperationId'))]",
-#             "properties": {
-#                 "displayName": "[parameters('OperationId')]",
-#                 "method": "POST",
-#                 "urlTemplate": "/hearing"
-#             }
-#         },
-#         {
-#             "type": "Microsoft.ApiManagement/service/apis/operations/policies",
-#             "apiVersion": "2019-12-01",
-#             "name": "[concat(parameters('ApimServiceName'), '/', parameters('ApiName'), '/', parameters('OperationId'), '/policy')]",
-#             "properties": {
-#                 "format": "rawxml-link",
-#                 "value": "https://raw.githubusercontent.com/hmcts/hmi-api-gateway-fh/HMIS-152_SANDBOX_CI/CD_Pipeline-temp/infrastructure/template/api-op-req-hearing-policy.xml"
-#             }
-#         }
-#     ],
-#     "outputs": {}
-# }
-# DEPLOY
-# }
+    template_body 	    = <<DEPLOY
+{
+    "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {
+        "apiName": {
+            "type": "String"
+        },
+        "apimServiceName": {
+            "type": "String"
+        },
+        "operationId": {
+            "type": "String"
+        }
+    },
+    "resources": [
+        {
+            "type": "Microsoft.ApiManagement/service/apis/operations",
+            "apiVersion": "2019-12-01",
+            "name": "[concat(parameters('apimServiceName'), '/', parameters('apiName'), '/', parameters('operationId'))]",
+            "properties": {
+                "displayName": "[parameters('operationId')]",
+                "method": "[parameters('method')]",
+                "urlTemplate": "[parameters('urlTemplate')]"
+            }
+        },
+        {
+            "type": "Microsoft.ApiManagement/service/apis/operations/policies",
+            "apiVersion": "2019-12-01",
+            "name": "[concat(parameters('apimServiceName'), '/', parameters('apiName'), '/', parameters('operationId'), '/policy')]",
+            "properties": {
+                "format": "[parameters('format')]",
+                "value": "[parameters('templateLink')]"
+            }
+        }
+    ],
+    "outputs": {}
+}
+DEPLOY
+}
