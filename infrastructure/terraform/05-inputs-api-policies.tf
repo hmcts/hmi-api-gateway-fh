bf7cc3410d609b0f0f41b727fe455cada511137c
karanotts <nottinghamkara@gmail.com>
2020-07-30 13:05:08 +0100

    test interpolation

--- infrastructure/terraform/05-inputs-api-policies.tf
+++ infrastructure/terraform/05-inputs-api-policies.tf
@@ -4,20 +4,18 @@ variable "api_policies" {
       operationId     = "request-hearing-1"
       apimServiceName = "azurerm_api_management.hmi_apim.name"
       apiName         = "azurerm_api_management_api.hmi_apim_api.name"
-      operationId     = "request-hearing"
       method          = "POST"
       format          = "rawxml-link"
-      urlTemplate     = "/hearings"
+      urlTemplate     = "/hearings1"
       templateLink    = "https://raw.githubusercontent.com/hmcts/hmi-api-gateway-fh/HMIS-152_SANDBOX_CI/CD_Pipeline-temp/infrastructure/template/api-op-req-hearing-policy.xml"
     },
         { 
       operationId     = "request-hearing-2"
       apimServiceName = "azurerm_api_management.hmi_apim.name"
       apiName         = "azurerm_api_management_api.hmi_apim_api.name"
-      operationId     = "request-hearing"
       method          = "POST"
       format          = "rawxml-link"
-      urlTemplate     = "/hearings"
+      urlTemplate     = "/hearings2"
       templateLink    = "https://raw.githubusercontent.com/hmcts/hmi-api-gateway-fh/HMIS-152_SANDBOX_CI/CD_Pipeline-temp/infrastructure/template/api-op-req-hearing-policy.xml"
     }
     ]
