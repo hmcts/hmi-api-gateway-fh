7d64e996e59f96a7cb582829b370d9ea370877a8
karanotts <nottinghamkara@gmail.com>
2020-07-30 09:13:26 +0100

    changing policy deployment to ARM

--- infrastructure/terraform/26-api-management-op-policy.tf
+++ infrastructure/terraform/26-api-management-op-policy.tf
@@ -1,11 +1,11 @@
-resource "azurerm_api_management_api_operation_policy" "request_hearing_policy" {
-  api_name            = azurerm_api_management_api.hmi_apim_api.name
-  api_management_name = azurerm_api_management.hmi_apim.name
-  resource_group_name = azurerm_resource_group.hmi_apim_rg.name
-  operation_id        = "request-hearing"
+# resource "azurerm_api_management_api_operation_policy" "request_hearing_policy" {
+#   api_name            = azurerm_api_management_api.hmi_apim_api.name
+#   api_management_name = azurerm_api_management.hmi_apim.name
+#   resource_group_name = azurerm_resource_group.hmi_apim_rg.name
+#   operation_id        = "request-hearing"
 
-  xml_content = file("../template/api-op-req-hearing-policy.xml")
-}
+#   xml_content = file("../template/api-op-req-hearing-policy.xml")
+# }
 
 resource "azurerm_api_management_api_operation_policy" "health_check_policy" {
   api_name            = azurerm_api_management_api.hmi_apim_api.name
