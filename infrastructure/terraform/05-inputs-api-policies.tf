ed8dbe9da3c000548a6b393749e166319d02fe3c
karanotts <nottinghamkara@gmail.com>
2020-07-30 12:37:42 +0100

    test ft outputs

--- infrastructure/terraform/05-inputs-api-policies.tf
+++ infrastructure/terraform/05-inputs-api-policies.tf
@@ -1,7 +1,17 @@
 variable "api_policies" {
     default = [
     { 
+      operationId     = "request-hearing-1"
+      apimServiceName = "azurerm_api_management.hmi_apim.name"
+      apiName         = "azurerm_api_management_api.hmi_apim_api.name"
       operationId     = "request-hearing"
+      method          = "POST"
+      format          = "rawxml-link"
+      urlTemplate     = "/hearings"
+      templateLink    = "https://raw.githubusercontent.com/hmcts/hmi-api-gateway-fh/HMIS-152_SANDBOX_CI/CD_Pipeline-temp/infrastructure/template/api-op-req-hearing-policy.xml"
+    },
+        { 
+      operationId     = "request-hearing-2"
       apimServiceName = "azurerm_api_management.hmi_apim.name"
       apiName         = "azurerm_api_management_api.hmi_apim_api.name"
       operationId     = "request-hearing"
