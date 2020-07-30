0d194f9280984c1041a8e667c25cc2e342db3a0a
karanotts <nottinghamkara@gmail.com>
2020-07-30 11:36:27 +0100

    mapping vars from list

--- infrastructure/terraform/05-inputs-api-policies.tf
+++ infrastructure/terraform/05-inputs-api-policies.tf
@@ -1,13 +1,13 @@
 variable "api_policies" {
     default = [
     { 
-      operationId     = "request-hearing",
-      apimServiceName = "azurerm_api_management.hmi_apim.name",
-      apiName         = "azurerm_api_management_api.hmi_apim_api.name",
-      operationId     = "request-hearing",
-      method          = "POST",
-      format          = "rawxml-link",
-      urlTemplate     = "/hearings",
+      operationId     = "request-hearing"
+      apimServiceName = "azurerm_api_management.hmi_apim.name"
+      apiName         = "azurerm_api_management_api.hmi_apim_api.name"
+      operationId     = "request-hearing"
+      method          = "POST"
+      format          = "rawxml-link"
+      urlTemplate     = "/hearings"
       templateLink    = "https://raw.githubusercontent.com/hmcts/hmi-api-gateway-fh/HMIS-152_SANDBOX_CI/CD_Pipeline-temp/infrastructure/template/api-op-req-hearing-policy.xml"
     }
     ]
