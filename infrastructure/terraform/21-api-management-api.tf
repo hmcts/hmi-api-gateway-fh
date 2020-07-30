bad0379ae3a0ea222229e596c261a72445a2d299
karanotts <nottinghamkara@gmail.com>
2020-07-30 15:30:19 +0100

    fixing arm template

--- infrastructure/terraform/21-api-management-api.tf
+++ infrastructure/terraform/21-api-management-api.tf
@@ -1,5 +1,5 @@
 resource "azurerm_api_management_api" "hmi_apim_api" {
-  name                = "${var.prefix}-${var.product}-api-${var.environment}"
+  name                = "${var.prefix}-${var.product}-api"
   resource_group_name = azurerm_resource_group.hmi_apim_rg.name
   api_management_name = azurerm_api_management.hmi_apim.name
   revision            = var.revision
