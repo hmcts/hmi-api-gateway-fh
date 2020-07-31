2cc98014dde1876b0af30578fb077cc831f057a2
karanotts <nottinghamkara@gmail.com>
2020-07-31 08:57:57 +0100

    removed unused vars

--- infrastructure/terraform/05-inputs-api-policies.tf
+++ infrastructure/terraform/05-inputs-api-policies.tf
@@ -2,18 +2,12 @@ variable "api_policies" {
     default = [
     { 
       operationId     = "Request-Hearing"
-      operationName   = "Request Hearing"
-      method          = "POST"
       format          = "rawxml-link"
-      urlTemplate     = "/hearings"
       templateFile    = "api-op-req-hearing-policy.xml"
     },
         { 
       operationId     = "Health-Check"
-      operationName   = "Health Check"
-      method          = "GET"
       format          = "rawxml-link"
-      urlTemplate     = "/"
       templateFile    = "api-op-health-check-policy.xml"
     }
     ]
