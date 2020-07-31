dc7677487b99d84c7bd27ff198f0d24fa725563f
karanotts <nottinghamkara@gmail.com>
2020-07-31 16:00:11 +0100

    add validation

--- infrastructure/terraform/01-inputs-shared.tf
+++ infrastructure/terraform/01-inputs-shared.tf
@@ -5,8 +5,8 @@ variable "location" {}
 variable "tags" {
   description = "The tags to associate with your resources."
   default = {
-    createdby = "HMI"
-    managedby = "HMI"
+    createdby   = "HMI"
+    managedby   = "HMI"
     application = "Azure APIM Management"
   }
 }
--- infrastructure/terraform/30-api-management-arm.tf
+++ infrastructure/terraform/30-api-management-arm.tf
@@ -1,17 +1,17 @@
 resource "azurerm_template_deployment" "apim-policy" {
-    name                = "apim-policy-${format("%02d",count.index)}"
-    resource_group_name = azurerm_resource_group.hmi_apim_rg.name
-    depends_on          = [azurerm_api_management_api.hmi_apim_api]
-    deployment_mode 	= "Incremental"
-    count               = length(var.api_policies)
-    parameters          = {
-        apimServiceName = azurerm_api_management.hmi_apim.name
-        apiName         = azurerm_api_management_api.hmi_apim_api.name
-        operationId     = lookup(var.api_policies[count.index], "operationId")
-        format          = lookup(var.api_policies[count.index], "format")
-        templateFile    = lookup(var.api_policies[count.index], "templateFile")
-    	}
-    template_body 	    = <<DEPLOY
+  name                = "apim-policy-${format("%02d", count.index)}"
+  resource_group_name = azurerm_resource_group.hmi_apim_rg.name
+  depends_on          = [azurerm_api_management_api.hmi_apim_api]
+  deployment_mode     = "Incremental"
+  count               = length(var.api_policies)
+  parameters = {
+    apimServiceName = azurerm_api_management.hmi_apim.name
+    apiName         = azurerm_api_management_api.hmi_apim_api.name
+    operationId     = lookup(var.api_policies[count.index], "operationId")
+    format          = lookup(var.api_policies[count.index], "format")
+    templateFile    = lookup(var.api_policies[count.index], "templateFile")
+  }
+  template_body = <<DEPLOY
 {
     "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
     "contentVersion": "1.0.0.0",
