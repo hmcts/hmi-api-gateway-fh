e4bd732592d49033573808e2f630f48233d9870c
SachinNaik <sachin.naik@hmcts.net>
2020-07-31 17:12:20 +0100

    Code cleanup

--- src/main/java/uk/gov/hmcts/futurehearings/hmi/Application.java
+++ src/main/java/uk/gov/hmcts/futurehearings/hmi/Application.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.futurehearings.hmi.functional;
+package uk.gov.hmcts.futurehearings.hmi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
