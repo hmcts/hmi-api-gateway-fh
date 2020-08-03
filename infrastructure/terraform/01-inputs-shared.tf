b9b5aa5d02891bfb935eb77a7ecf60ba5053da32
karanotts <nottinghamkara@gmail.com>
2020-08-03 12:55:19 +0100

    adding tags

--- infrastructure/terraform/01-inputs-shared.tf
+++ infrastructure/terraform/01-inputs-shared.tf
@@ -2,11 +2,4 @@ variable "prefix" {}
 variable "product" {}
 variable "environment" {}
 variable "location" {}
-variable "tags" {
-  description = "The tags to associate with your resources."
-  default = {
-    createdby = "HMI"
-    managedby = "HMI"
-    application = "Azure APIM Management"
-  }
-}
+variable "tags" {}
