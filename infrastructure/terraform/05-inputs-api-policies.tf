c527e60401627f6ffe68f3e3d9c3e09c3ea92554
karanotts <nottinghamkara@gmail.com>
2020-08-05 10:21:21 +0100

    corrected tf formatting

--- infrastructure/terraform/05-inputs-api-policies.tf
+++ infrastructure/terraform/05-inputs-api-policies.tf
@@ -1,14 +1,14 @@
 variable "api_policies" {
-    default = [
-    { 
-      operationId     = "Request-Hearing"
-      format          = "rawxml-link"
-      templateFile    = "api-op-request-hearing-policy.xml"
+  default = [
+    {
+      operationId  = "Request-Hearing"
+      format       = "rawxml-link"
+      templateFile = "api-op-request-hearing-policy.xml"
     },
-        { 
-      operationId     = "Health-Check"
-      format          = "rawxml-link"
-      templateFile    = "api-op-health-check-policy.xml"
+    {
+      operationId  = "Health-Check"
+      format       = "rawxml-link"
+      templateFile = "api-op-health-check-policy.xml"
     }
-    ]
+  ]
 }
