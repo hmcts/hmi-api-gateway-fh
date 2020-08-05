5d4fff8ad493df8e176bcc375f0c6e0db84d0d72
463952 <subroto.mukherjee@HMCTS.NET>
2020-08-05 10:20:30 +0100

    HMIS-174: Test demo

--- src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/steps/HearingApiCallSteps.java
+++ src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/steps/HearingApiCallSteps.java
@@ -11,8 +11,7 @@ public class HearingApiCallSteps {
     private String actor;
 
     @Step("#actor routes to {0} in order to get invoke {1}")
-    public Response requestHearingWithMissingField(final String api, final Map<String, Object> headersAsMap,
-                                                   final String basePath, final String payloadBody) {
+    public Response requestHearingWithMissingField(final String api, final Map<String, Object> headersAsMap, final String basePath, final String payloadBody) {
         return expect().that().statusCode(400)
                 .given().contentType("application/json").body(payloadBody)
                 .headers(headersAsMap)
