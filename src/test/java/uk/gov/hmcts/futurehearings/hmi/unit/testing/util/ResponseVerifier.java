d6a6070aab3f3dc131a4286289156d07fe516649
463952 <subroto.mukherjee@HMCTS.NET>
2020-08-06 17:35:18 +0100

    HMIS-219: Reverting the changes

--- src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/util/ResponseVerifier.java
+++ src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/util/ResponseVerifier.java
@@ -15,123 +15,122 @@ public class ResponseVerifier {
 
     public static void thenResponseHasErrorForMissingCaseTitle(Response response) {
         //Option 1 - Use JsonPath - (Native Matcher to RestAssured and Serenity Rest)
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        System.out.println("Response Map: " + responseMap);
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Case Title'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Case Title'",responseMap.get(("Reason")));
     }
 
     public static void thenResponseHasErrorForMissingCaseId(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Case Id'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Case Id'",responseMap.get(("Reason")));
     }
 
     public static void thenResponseHasErrorForMissingTransactionIDHMCTS(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Case Id'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Case Id'",responseMap.get(("Reason")));
     }
 
     public static void thenResponseHasErrorForMissingVenue(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Venue'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Venue'",responseMap.get(("Reason")));
     }
 
     public static void thenResponseHasErrorForMissingJurisdiction(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Jurisdiction'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Jurisdiction'",responseMap.get(("Reason")));
     }
 
     public static void thenResponseHasErrorForMissingService(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Service'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Service'",responseMap.get(("Reason")));
     }
 
-    public static void thenResponseHasErrorForMissingCaseType(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+    public static void thenResponseHasErrorForMissingCaseType (Response response) {
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Case Type'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Case Type'",responseMap.get(("Reason")));
     }
 
     public static void thenResponseHasErrorForMissingHearingType(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Hearing Type'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Hearing Type'",responseMap.get(("Reason")));
     }
 
     public static void thenResponseHasErrorForMissingPriority(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Priority'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Priority'",responseMap.get(("Reason")));
     }
 
     public static void thenResponseHasErrorForMissingStatusOfTheListingRequest(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Status of the Listing Request'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Status of the Listing Request'",responseMap.get(("Reason")));
     }
 
     public static void thenResponseHasErrorForMissingTicketTypeRequired(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Ticket Type required'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Ticket Type required'",responseMap.get(("Reason")));
     }
 
     public static void thenResponseHasErrorForMissingWaitingTimeKPI(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Waiting time KPI'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Waiting time KPI'",responseMap.get(("Reason")));
     }
 
     public static void thenResponseHasErrorForMissingReasonForSTLChange(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Reason for STL change'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Reason for STL change'",responseMap.get(("Reason")));
     }
 
     public static void thenResponseHasErrorForMissingEstimatedDurationOfHearing(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Estimated Duration of the Hearing'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Estimated Duration of the Hearing'",responseMap.get(("Reason")));
     }
 
     public static void thenResponseHasErrorForMissingHearingChannel(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Hearing channel'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Hearing channel'",responseMap.get(("Reason")));
     }
 
     public static void thenResponseHasErrorForMissingPrivateHearing(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Private Hearing'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Private Hearing'",responseMap.get(("Reason")));
     }
 
     public static void thenResponseHasErrorForMissingAllocatedListingTeam(Response response) {
-        assertEquals(2, response.getBody().jsonPath().getMap("$").size());
+        assertEquals(2,response.getBody().jsonPath().getMap("$").size());
         Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
-        assertEquals("400", responseMap.get(("Error")));
-        assertEquals("Malformed request. Missing/Invalid property: 'Allocated Listing Team'", responseMap.get(("Reason")));
+        assertEquals("400",responseMap.get(("Error")));
+        assertEquals("Malformed request. Missing/Invalid property: 'Allocated Listing Team'",responseMap.get(("Reason")));
     }
 
 
@@ -142,7 +141,7 @@ public class ResponseVerifier {
                     response.getBody().asString(), JSONCompareMode.STRICT);
         } catch (JSONException jsonException) {
             throw new AssertionError("Payloads have not matched");
-        } catch (IOException ioException) {
+        } catch (IOException ioException ) {
             throw new AssertionError("Response file cannot be read..");
         }
     }
