d16f6e09911ec1050cf81e01dd777c328c5d6f4f
SachinNaik <sachin.naik@hmcts.net>
2020-08-07 12:42:23 +0100

    review fixes

--- src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/hearing/HearingApiSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/hearing/HearingApiSmokeTest.java
@@ -3,11 +3,8 @@ package uk.gov.hmcts.futurehearings.hmi.smoke.hearing;
 import static io.restassured.RestAssured.expect;
 
 import uk.gov.hmcts.futurehearings.hmi.Application;
+import uk.gov.hmcts.futurehearings.hmi.smoke.SmokeTest;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,34 +14,10 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = {Application.class})
 @ActiveProfiles("smoke")
-public class HearingApiSmokeTest {
-
-    @Value("${targetInstance}")
-    private String targetInstance;
-
-    @Value("${targetHost}")
-    private String targetHost;
-
-    @Value("${targetSubscriptionKey}")
-    private String targetSubscriptionKey;
+public class HearingApiSmokeTest extends SmokeTest {
 
     @Value("${hearingApiRootContext}")
     private String hearingApiRootContext;
-
-    private Map<String, Object> headersAsMap = new HashMap<>();
-
-    @BeforeEach
-    public void initialiseValues() {
-        headersAsMap.put("Host", targetHost);
-        headersAsMap.put("Ocp-Apim-Subscription-Key", targetSubscriptionKey);
-        headersAsMap.put("Ocp-Apim-Trace", "true");
-        headersAsMap.put("Company-Name", "HMCTS");
-        headersAsMap.put("Content-Type", "application/json");
-        headersAsMap.put("Source", "SnL");
-        headersAsMap.put("Destination", "CFT");
-        headersAsMap.put("DateTime", "datetimestring");
-        headersAsMap.put("RequestType", "TypeOfCase");
-    }
 
     @Test
     @Disabled
