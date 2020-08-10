e059e47120ff02bc1b3e149752db3f5f5e7969c6
karanotts <nottinghamkara@gmail.com>
2020-08-10 15:55:48 +0100

    fmt

--- infrastructure/terraform/05-inputs-api-policies.tf
+++ infrastructure/terraform/05-inputs-api-policies.tf
@@ -1,5 +1,4 @@
 variable "api_policies" {
-<<<<<<< HEAD
   default = [
     {
       operationId  = "request-hearing"
@@ -10,18 +9,6 @@ variable "api_policies" {
       operationId  = "health-check"
       format       = "rawxml-link"
       templateFile = "api-op-health-check-policy.xml"
-=======
-    default = [
-    { 
-      operationId     = "request-hearing"
-      format          = "rawxml-link"
-      templateFile    = "api-op-request-hearing-policy.xml"
-    },
-        { 
-      operationId     = "health-check"
-      format          = "rawxml-link"
-      templateFile    = "api-op-health-check-policy.xml"
->>>>>>> c67044d8205b97556a2e3d86c264ac3f20d870c6
     }
   ]
 }
