3df1203722b43a30f85e311ac5a32cb3e0b2e4fc
santhoshhmcts <santhosh.radhakrishnan@HMCTS.NET>
2020-08-13 16:02:58 +0100

    HMIS-94 - Renamed headers to match LLD

--- src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/HmiApiSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/HmiApiSmokeTest.java
@@ -41,10 +41,10 @@ public class HmiApiSmokeTest {
         headersAsMap.put("Ocp-Apim-Trace", "true");
         headersAsMap.put("Company-Name", "HMCTS");
         headersAsMap.put("Content-Type", "application/json");
-        headersAsMap.put("Source", "SnL");
-        headersAsMap.put("Destination", "CFT");
-        headersAsMap.put("DateTime", "datetimestring");
-        headersAsMap.put("RequestType", "TypeOfCase");
+        headersAsMap.put("Source-System", "SnL");
+        headersAsMap.put("Destination-System", "CFT");
+        headersAsMap.put("Request-Created-At", "datetimestring");
+        headersAsMap.put("Request-Type", "TypeOfCase");
     }
 
     @Test
