2d31e55c2ceda322af5668ff7109b634d7773e02
Pettedson John <pettedson.john@hmcts.net>
2020-08-13 11:24:29 +0100

    Adding all the Tests and Code for the Wiremock Verification

--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/schedule/ScheduleValidationTest.java
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/schedule/ScheduleValidationTest.java
@@ -3,11 +3,14 @@ package uk.gov.hmcts.futurehearings.hmi.acceptance.schedule;
 import uk.gov.hmcts.futurehearings.hmi.Application;
 import uk.gov.hmcts.futurehearings.hmi.acceptance.delegate.Schedule;
 
+import io.restassured.RestAssured;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,7 +20,8 @@ import org.springframework.test.context.ActiveProfiles;
 @Slf4j
 @SpringBootTest(classes = {Application.class})
 @ActiveProfiles("acceptance")
-public class ScheduleAcceptanceTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ScheduleValidationTest {
 
     @Value("${targetInstance}")
     private String targetInstance;
@@ -25,23 +29,32 @@ public class ScheduleAcceptanceTest {
     @Value("${targetSubscriptionKey}")
     private String targetSubscriptionKey;
 
+    @Value("${hearingApiRootContext}")
+    private String hearingAPIRootContext;
+
     @Autowired
     private Schedule scheduleDelegate;
 
+    @BeforeAll
+    public void initialiseValues() {
+        RestAssured.baseURI = targetInstance;
+        RestAssured.useRelaxedHTTPSValidation();
+    }
+
     @BeforeEach
     public void beforeEach(TestInfo info) {
-        System.out.println("Before execute : "+info.getTestMethod().get().getName());
-        System.out.println("The value of the targetInstance "+targetInstance);
+        log.debug("Before execute : "+info.getTestMethod().get().getName());
+        log.debug("The value of the targetInstance "+targetInstance);
     }
 
     @AfterEach
     public void afterEach(TestInfo info) {
-        System.out.println("After execute : "+info.getTestMethod().get().getName());
+        log.debug("After execute : "+info.getTestMethod().get().getName());
     }
 
     @Test
     public void test_sucessfull_post() throws Exception {
         scheduleDelegate.test_successfull_post(targetSubscriptionKey,
-                   targetInstance);
+                hearingAPIRootContext);
     }
 }
