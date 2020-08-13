d793e6a496422da0f8a0002d1404f7f0a93dc7b0
Pettedson John <pettedson.john@hmcts.net>
2020-08-13 11:24:29 +0100

    Adding all the Tests and Code for the Wiremock Verification

--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/common/RestTemplate.java
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/common/RestTemplate.java
@@ -5,9 +5,10 @@ import java.util.Map;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-
+@Slf4j
 public class RestTemplate {
 
     public static Response shouldExecutePost(final Map<String,String> headersAsMap,
@@ -15,12 +16,15 @@ public class RestTemplate {
                                              final String requestURL,
                                              final HttpStatus expectedHttpStatus) {
 
+       log.debug("The value of the baseURI"+RestAssured.baseURI);
+
         return RestAssured.expect().that().statusCode(expectedHttpStatus.value())
                 .given()
                 .headers(headersAsMap)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .basePath(requestURL)
                 .body(requestBodyPayload)
                 .when()
-                .post(requestURL).then().extract().response();
+                .post().then().extract().response();
     }
 }
