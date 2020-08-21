4afa657ebc01a2fedf0bcaf93bcf3a23cb864ea3
Pettedson John <pettedson.john@hmcts.net>
2020-08-21 12:53:43 +0100

    Added all the Respective changes to accomodate the headers in for HMIS-97

--- src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/updateuser/UserApiSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/updateuser/UserApiSmokeTest.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.futurehearings.hmi.smoke.updateuser;
 
-import static io.restassured.RestAssured.expect;
+import static io.restassured.RestAssured.given;
 
 import uk.gov.hmcts.futurehearings.hmi.Application;
 import uk.gov.hmcts.futurehearings.hmi.smoke.SmokeTest;
 
+import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,23 +14,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+@Slf4j
 @SpringBootTest(classes = {Application.class})
 @ActiveProfiles("smoke")
-@Disabled("For the purpose of adding the acceptance Test initial framework")
+@Disabled("For as the endpoint for User API is not available")
 public class UserApiSmokeTest extends SmokeTest {
 
     @Value("${userApiRootContext}")
     public String userApiRootContext;
 
     @Test
-    @Disabled
     @DisplayName("Smoke Test to Test the Endpoint for the Get Update Users Root Context")
     public void testSuccessfulAllSessionsApiGet() {
-         expect().that().statusCode(200)
-                .given().contentType("application/json")
+        Response response = given()
                 .headers(headersAsMap)
-                .baseUri(targetInstance)
                 .basePath(userApiRootContext)
                 .when().get();
+
+        if (response.getStatusCode() != 200) {
+            log.info(" The value of the Response Status " + response.getStatusCode());
+            log.info(" The value of the Response body " + response.getBody().prettyPrint());
+        }
     }
 }
