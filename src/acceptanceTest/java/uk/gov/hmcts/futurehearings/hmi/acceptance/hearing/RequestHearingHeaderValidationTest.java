82ab62ac46b546b9ae0b1eb4525c5e52f1ac62b8
Pettedson John <pettedson.john@hmcts.net>
2020-08-23 15:08:03 +0100

    Changed the Framework in order to accomodate GET's in the RestTemplate and Sessions as well

--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/hearing/RequestHearingHeaderValidationTest.java
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/hearing/RequestHearingHeaderValidationTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 
 
@@ -31,13 +32,14 @@ public class RequestHearingHeaderValidationTest extends HearingValidationTest {
     @Value("${hearingApiRootContext}")
     private String hearingAPIRootContext;
 
-    private HttpMethod httpMethod;
-
     @BeforeAll
     public void initialiseValues() {
         super.initialiseValues();
         this.setRelativeURL(hearingAPIRootContext);
         this.setHttpMethod(HttpMethod.POST);
         this.setInputPayloadFileName("hearing-request-standard.json");
+        this.setHttpSucessStatus(HttpStatus.OK);
+        this.setRelativeURLForNotFound(this.getRelativeURL().replace("hearings","hearing"));
     }
+
 }
