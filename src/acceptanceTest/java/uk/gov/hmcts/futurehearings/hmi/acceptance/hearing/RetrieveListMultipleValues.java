09c286a7ec949599b27d6c7931a7286766f92c60
Pettedson John <pettedson.john@hmcts.net>
2020-09-03 11:33:57 +0100

    Added the RetrieveList Multiple Values Test Initial Cut

---
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/hearing/RetrieveListMultipleValues.java
@@ -0,0 +1,38 @@
+package uk.gov.hmcts.futurehearings.hmi.acceptance.hearing;
+
+import uk.gov.hmcts.futurehearings.hmi.Application;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.sessions.SessionsValidationTest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+
+@Slf4j
+@SpringBootTest(classes = {Application.class})
+@ActiveProfiles("acceptance")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class RetrieveListMultipleValues extends SessionsValidationTest {
+    @Value("${targetInstance}")
+    private String targetInstance;
+
+    @Value("${targetSubscriptionKey}")
+    private String targetSubscriptionKey;
+
+    @Value("${hearingApiRootContext}")
+    private String hearingAPIRootContext;
+
+    @BeforeAll
+    public void initialiseValues() {
+        super.initialiseValues();
+        this.setRelativeURL(hearingAPIRootContext);
+        this.setHttpMethod(HttpMethod.GET);
+        this.setHttpSucessStatus(HttpStatus.OK);
+        this.setRelativeURLForNotFound(this.getRelativeURL().replace("hearings","hearing"));
+        //this.setUrlParams(buildValidRetrieveScheduleParams());
+    }
+}
