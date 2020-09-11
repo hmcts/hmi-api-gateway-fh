26c4938fba004be6669a361efcca060709853e0f
karanotts <nottinghamkara@gmail.com>
2020-09-11 16:02:34 +0100

    mv cert to local vault

--- infrastructure/terraform/14-custom-domain.tf
+++ infrastructure/terraform/14-custom-domain.tf
@@ -2,12 +2,16 @@ locals {
   hostname_configuration_proxy = {
     default_ssl_binding = true
     host_name           = var.host_name
-    key_vault_id        = var.key_vault_id
     certificate_name    = var.certificate_name
   }
 }
 
+data "azurerm_key_vault" "cert_key_vault" {
+  name                = var.key_vault_name
+  resource_group_name = var.key_vault_rg
+}
+
 data "azurerm_key_vault_secret" "certificate" {
   name         = local.hostname_configuration_proxy.certificate_name
-  key_vault_id = local.hostname_configuration_proxy.key_vault_id
+  key_vault_id = data.azurerm_key_vault.cert_key_vault.id
 }
