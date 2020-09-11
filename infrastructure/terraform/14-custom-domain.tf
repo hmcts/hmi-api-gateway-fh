55c8b351161be3a68f85f5597bc0ca3262c2d281
karanotts <nottinghamkara@gmail.com>
2020-09-11 15:20:08 +0100

    correct locals

--- infrastructure/terraform/14-custom-domain.tf
+++ infrastructure/terraform/14-custom-domain.tf
@@ -1,5 +1,5 @@
 locals {
-  hostname_configuration_proxy {
+  hostname_configuration_proxy = {
     default_ssl_binding = true
     host_name           = var.host_name
     key_vault_id        = var.key_vault_id
@@ -8,6 +8,6 @@ locals {
 }
 
 data "azurerm_key_vault_secret" "certificate" {
-  name         = var.hostname_configuration_proxy.certificate_name
-  key_vault_id = var.hostname_configuration_proxy.key_vault_id
+  name         = local.hostname_configuration_proxy.certificate_name
+  key_vault_id = local.hostname_configuration_proxy.key_vault_id
 }
