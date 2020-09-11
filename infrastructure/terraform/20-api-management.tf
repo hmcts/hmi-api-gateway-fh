bc7dd9dd6d08b02c0e2a73da7f691fdde23d362d
karanotts <nottinghamkara@gmail.com>
2020-09-11 18:07:56 +0100

    add mi

--- infrastructure/terraform/20-api-management.tf
+++ infrastructure/terraform/20-api-management.tf
@@ -7,17 +7,22 @@ resource "azurerm_api_management" "hmi_apim" {
   sku_name            = "${var.apim_sku_name}_${var.apim_sku_capacity}"
   tags                = var.tags
 
+  identity {
+    type = "SystemAssigned"
+    principal_id = var.principal_object_id
+    tenant_id =  var.principal_tenant_id
+  }
+  
   virtual_network_type = var.virtual_network_type
-
   virtual_network_configuration {
     subnet_id = data.azurerm_subnet.hmi_apim_subnet.id
   }
 
   hostname_configuration {
     proxy {
-        default_ssl_binding = true
-        host_name           = var.host_name
-        key_vault_id        = replace(data.azurerm_key_vault_secret.certificate_secret.id, "/${data.azurerm_key_vault_secret.certificate_secret.version}", "")
+      default_ssl_binding = true
+      host_name           = var.host_name
+      key_vault_id        = replace(data.azurerm_key_vault_secret.certificate_secret.id, "/${data.azurerm_key_vault_secret.certificate_secret.version}", "")
     }
   }
 }
