418a117d65a802313d4132e808094fd5b6cc948f
karanotts <nottinghamkara@gmail.com>
2020-09-15 10:38:21 +0100

    rm sharedkvperm

--- infrastructure/terraform/14-domain-cert.tf
+++ infrastructure/terraform/14-domain-cert.tf
@@ -3,16 +3,6 @@ data "azurerm_key_vault" "cert_key_vault" {
   resource_group_name = var.key_vault_rg
 }
 
-resource "azurerm_key_vault_access_policy" "infra_kv_permissions" {
-  key_vault_id            = data.azurerm_key_vault.cert_key_vault.id
-  tenant_id               = azurerm_key_vault.hmi_apim_kv.tenant_id
-  object_id               = var.principal_object_id
-  certificate_permissions = var.certificate_permissions
-  key_permissions         = var.key_permissions
-  secret_permissions      = var.secret_permissions
-  storage_permissions     = var.storage_permissions
-}
-
 data "azurerm_key_vault_secret" "certificate_secret" {
   name         = "STAR-sandbox-platform-hmcts-net"
   key_vault_id = data.azurerm_key_vault.cert_key_vault.id
