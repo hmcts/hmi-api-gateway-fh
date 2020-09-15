3210730fb46bebf9373d6f6f23fddc841c6b89db
karanotts <nottinghamkara@gmail.com>
2020-09-15 08:54:00 +0100

    connect infra kv

--- infrastructure/terraform/14-domain-cert.tf
+++ infrastructure/terraform/14-domain-cert.tf
@@ -3,6 +3,16 @@ data "azurerm_key_vault" "cert_key_vault" {
   resource_group_name = var.key_vault_rg
 }
 
+resource "azurerm_key_vault_access_policy" "permissions" {
+  key_vault_id            = data.azurerm_key_vault.cert_key_vault.id
+  tenant_id               = azurerm_key_vault.hmi_apim_kv.tenant_id
+  object_id               = var.principal_object_id
+  certificate_permissions = var.certificate_permissions
+  key_permissions         = var.key_permissions
+  secret_permissions      = var.secret_permissions
+  storage_permissions     = var.storage_permissions
+}
+
 data "azurerm_key_vault_secret" "certificate_secret" {
   name         = var.certificate_name
   key_vault_id = data.azurerm_key_vault.cert_key_vault.id
