2c34e9da2dc618b74dd48f30907aeee38949ae21
Pettedson John <pettedson.john@hmcts.net>
2020-09-16 22:16:25 +0100

    Addig all the Test files for Verify after Testing the HMICommonTest code

--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/resources/GETResourcesValidationTest.java
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/resources/GETResourcesValidationTest.java
@@ -1,11 +1,12 @@
 package uk.gov.hmcts.futurehearings.hmi.acceptance.resources;
 
 import uk.gov.hmcts.futurehearings.hmi.Application;
-import uk.gov.hmcts.futurehearings.hmi.acceptance.hearings.HearingValidationTest;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.error.HMICommonErrorVerifier;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.resources.verify.GETResourceByIDValidationVerifier;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.resources.verify.GETResourcesValidationVerifier;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,7 +18,6 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest(classes = {Application.class})
 @ActiveProfiles("acceptance")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@Disabled
 public class GETResourcesValidationTest extends ResourceValidationTest {
 
     @Value("${targetInstance}")
@@ -36,5 +36,7 @@ public class GETResourcesValidationTest extends ResourceValidationTest {
         this.setHttpMethod(HttpMethod.GET);
         this.setHttpSucessStatus(HttpStatus.OK);
         this.setRelativeURLForNotFound(this.getRelativeURL().replace("resources","resource"));
+        this.setHmiSuccessVerifier(new GETResourcesValidationVerifier());
+        this.setHmiErrorVerifier(new HMICommonErrorVerifier());
     }
 }
