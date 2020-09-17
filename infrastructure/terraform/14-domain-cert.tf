c3ffdf9d8e4afa94740f14cb4d7786b9c93730e2
karanotts <nottinghamkara@gmail.com>
2020-09-17 16:22:00 +0100

    rm mi

--- infrastructure/terraform/14-domain-cert.tf
+++
@@ -1,9 +0,0 @@
-data "azurerm_key_vault" "cert_key_vault" {
-  name                = var.key_vault_name
-  resource_group_name = var.key_vault_rg
-}
-
-data "azurerm_key_vault_secret" "certificate_secret" {
-  name         = "STAR-sandbox-platform-hmcts-net"
-  key_vault_id = data.azurerm_key_vault.cert_key_vault.id
-}
