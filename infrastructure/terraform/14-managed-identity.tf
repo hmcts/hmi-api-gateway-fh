7c1e5b6398a41990a9bdbb07908c6bd73e86df15
karanotts <nottinghamkara@gmail.com>
2020-09-17 16:22:00 +0100

    rm mi

--- infrastructure/terraform/14-managed-identity.tf
+++
@@ -1,4 +0,0 @@
-data "azurerm_user_assigned_identity" "hmi_apim_mi" {
-  name                = "hmi-sharedinfra-mi-${var.environment}"
-  resource_group_name = "hmi-sharedinfra-${var.environment}-rg"
-}
