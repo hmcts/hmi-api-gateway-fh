fe8b7301894927c3a7717c23677802198a2ab407
karanotts <nottinghamkara@gmail.com>
2020-09-21 13:44:14 +0100

    domain apply ps1

---
+++ script/apply_custom_domain.ps1
@@ -0,0 +1,19 @@
+[CmdletBinding()]
+Param (
+[Parameter(Mandatory=$true)]
+[string] $Hostname,
+
+[Parameter(Mandatory=$true)]
+[string] $HostnameType,
+
+[Parameter(Mandatory=$true)]
+[string] $KeyVaultId,
+
+[Parameter(Mandatory=$true)]
+[string] $ResourceGroupName
+)
+
+$proxy = New-AzApiManagementCustomHostnameConfiguration -Hostname $Hostname -HostnameType $HostnameType -KeyVaultId $KeyVaultId -DefaultSslBinding
+$apim = Get-AzApiManagement -ResourceGroupName $ResourceGroupName
+$apim.ProxyCustomHostnameConfiguration = $proxy
+Set-AzApiManagement -InputObject $apim -SystemAssignedIdentity
