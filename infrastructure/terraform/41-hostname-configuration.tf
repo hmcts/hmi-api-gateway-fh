077220143314c3f872d3e073e35654b07610b91b
karanotts <nottinghamkara@gmail.com>
2020-09-21 14:09:11 +0100

    run tf on win

--- infrastructure/terraform/41-hostname-configuration.tf
+++ infrastructure/terraform/41-hostname-configuration.tf
@@ -1,14 +1,7 @@
-# resource "null_resource" "run-posh" {
-#   provisioner "local-exec" {
-#     command = "../../script/apply_custom_domain.ps1 -Hostname '${var.custom_hostname}' -HostnameType '${var.custom_hostname_type}' -KeyVaultId '${var.custom_hostname_certificate}' -ResourceGroupName '${var.infra_kv_name}"
-#     interpreter = ["PowerShell", "-Command"]
-#   }
-#   depends_on = []
-# }
-
-resource "null_resource" "example2" {
+resource "null_resource" "run-posh" {
   provisioner "local-exec" {
-    command = "Get-Date > completed.txt"
+    command = "../../script/apply_custom_domain.ps1 -Hostname '${var.custom_hostname}' -HostnameType '${var.custom_hostname_type}' -KeyVaultId '${var.custom_hostname_certificate}' -ResourceGroupName '${var.infra_kv_name}"
     interpreter = ["PowerShell", "-Command"]
   }
+  depends_on = [azurerm_api_management.hmi_apim]
 }
