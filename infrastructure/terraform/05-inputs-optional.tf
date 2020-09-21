75cba9014baaf83b992813da6487a879bb0b288e
karanotts <nottinghamkara@gmail.com>
2020-09-21 13:44:31 +0100

    try local exec domain apply

--- infrastructure/terraform/05-inputs-optional.tf
+++ infrastructure/terraform/05-inputs-optional.tf
@@ -4,3 +4,12 @@ variable "infra_kv_name" {
 variable "infra_kv_rg" {
   default = null
 }
+variable "custom_hostname" {
+  default = "hmi-apim-sbox.sandbox.platform.hmcts.net"
+}
+variable "custom_hostname_type" {
+  default = "Proxy"
+}
+variable "custom_hostname_certificate" {
+  default = "https://hmi-sharedinfra-kv-sbox.vault.azure.net/secrets/temp-cert"
+}
