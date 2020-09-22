ccc8a0187d7731d4d2de955e84b134fbfc63bb06
Pettedson John <pettedson.john@hmcts.net>
2020-09-22 10:51:41 +0100

    Added the acceptance tests for the Delete Resource Feature...

---
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/resources/DELETEResourcesValidationTest.java
@@ -0,0 +1,72 @@
+package uk.gov.hmcts.futurehearings.hmi.acceptance.resources;
+
+import static uk.gov.hmcts.futurehearings.hmi.acceptance.common.helper.CommonHeaderHelper.createCompletePayloadHeader;
+import static uk.gov.hmcts.futurehearings.hmi.acceptance.common.helper.CommonHeaderHelper.createStandardPayloadHeader;
+
+import uk.gov.hmcts.futurehearings.hmi.Application;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.common.delegate.CommonDelegate;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.error.HMICommonErrorVerifier;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.success.HMICommonSuccessVerifier;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.hearings.HearingValidationTest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@Slf4j
+@SpringBootTest(classes = {Application.class})
+@ActiveProfiles("acceptance")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SelectClasses(DELETEResourcesValidationTest.class)
+@IncludeTags("Delete")
+public class DELETEResourcesValidationTest extends ResourceValidationTest {
+
+    @Qualifier("CommonDelegate")
+    @Autowired(required = true)
+    private CommonDelegate commonDelegate;
+
+    @Value("${resources_idRootContext}")
+    private String resources_idRootContext;
+
+    @BeforeAll
+    public void initialiseValues() {
+        super.initialiseValues();
+        resources_idRootContext = String.format(resources_idRootContext,"12345");
+        this.setRelativeURL(resources_idRootContext);
+        this.setHttpMethod(HttpMethod.DELETE);
+        this.setInputPayloadFileName("delete-resource-request-valid.json");
+        this.setHttpSucessStatus(HttpStatus.OK);
+        this.setRelativeURLForNotFound(this.getRelativeURL().replace("resources","resource"));
+        this.setHmiSuccessVerifier(new HMICommonSuccessVerifier());
+        this.setHmiErrorVerifier(new HMICommonErrorVerifier());
+    }
+
+    //This test is for a Standard Header but a Payload for Non JSON Type is to be tested.
+    //Confirmed by Product Owner that this should be a Success Scenario.
+    @Test
+    @DisplayName("Successfully validated response with an xml payload")
+    public void test_successful_response_for_test_xml_body() throws Exception {
+
+        commonDelegate.test_expected_response_for_supplied_header(getApiSubscriptionKey(),
+                getRelativeURL(), "sample-xml-payload.xml",
+                createStandardPayloadHeader(getApiSubscriptionKey()),
+                null,
+                getUrlParams(),
+                getHttpMethod(),
+                this.getHttpSucessStatus(),
+                "common",
+                getHmiSuccessVerifier(),"The request was received successfully.");
+    }
+}
