3e42b3509f0555d556ec5ec7bd93a251e66beac3
Pettedson John <pettedson.john@hmcts.net>
2020-09-22 09:56:39 +0100

    Changed the Header Map to Wiremock Header post Changes for Duplicate Headers in the API calls....

--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/common/delegate/CommonDelegateImpl.java
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/common/delegate/CommonDelegateImpl.java
@@ -6,8 +6,12 @@ import uk.gov.hmcts.futurehearings.hmi.acceptance.common.TestingUtils;
 import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.HMIVerifier;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
+import io.restassured.http.Header;
 import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
@@ -20,10 +24,11 @@ import org.springframework.stereotype.Component;
 public class CommonDelegateImpl implements CommonDelegate {
 
     private static final String INPUT_FILE_PATH = "uk/gov/hmcts/futurehearings/hmi/acceptance/%s/input";
+
     public void test_expected_response_for_supplied_header(final String targetSubscriptionKey,
                                                            final String targetURL,
                                                            final String inputFile,
-                                                           final Map<String,String> standardHeaderMap,
+                                                           final Map<String, String> standardHeaderMap,
                                                            final Headers headers,
                                                            final Map<String, String> params,
                                                            final HttpMethod httpMethod,
@@ -32,32 +37,68 @@ public class CommonDelegateImpl implements CommonDelegate {
                                                            final HMIVerifier hmiVerifier,
                                                            final String expectedMessage) throws IOException {
 
-        log.debug("The value of the target header" +standardHeaderMap);
+        log.debug("The value of the target header (Header Map) : " + standardHeaderMap);
+        log.debug("The value of the target header (Wiremock Header) :" + standardHeaderMap);
+        Headers standardWireMockHeaders = null;
+        if (Objects.nonNull(standardHeaderMap) && standardHeaderMap.size() > 0) {
+            standardWireMockHeaders = convertHeaderMapToWireMockHeaders(standardHeaderMap);
+        } else {
+            standardWireMockHeaders = headers;
+        }
+        handleRestCall(targetURL, inputFile, standardWireMockHeaders, params, httpMethod, status, inputFileDirectory, hmiVerifier, expectedMessage);
+
+    }
+
+    private void handleRestCall(final String targetURL,
+                                final String inputFile,
+                                final Headers headers,
+                                final Map<String, String> params,
+                                final HttpMethod httpMethod,
+                                final HttpStatus status,
+                                final String inputFileDirectory,
+                                final HMIVerifier hmiVerifier,
+                                final String expectedMessage) throws IOException {
+
         String inputPayload = null;
         switch (httpMethod) {
             case POST:
             case PUT:
             case DELETE:
                 inputPayload = TestingUtils.readFileContents(String.format(INPUT_FILE_PATH, inputFileDirectory) + "/" + inputFile);
-                hmiVerifier.verify(status,expectedMessage,
-                        performRESTCall(targetURL, standardHeaderMap, headers, params, httpMethod, status, inputPayload));
+                hmiVerifier.verify(status, expectedMessage,
+                        performRESTCall(targetURL, headers, params, httpMethod, status, inputPayload));
                 break;
             case GET:
-                hmiVerifier.verify(status,expectedMessage,performRESTCall(targetURL, standardHeaderMap, headers, params, httpMethod, status, inputPayload));
+                hmiVerifier.verify(status, expectedMessage, performRESTCall(targetURL, headers, params, httpMethod, status, inputPayload));
                 break;
             case OPTIONS:
-                performRESTCall(targetURL, standardHeaderMap, headers, params, httpMethod, status, inputPayload);
+                performRESTCall(targetURL, headers, params, httpMethod, status, inputPayload);
         }
-
     }
 
-    private Response performRESTCall(final String targetURL, final Map<String, String> standardHeaderMap, final Headers headers, final Map<String, String> params, final HttpMethod httpMethod, final HttpStatus status, final String inputPayload) {
-        return shouldExecute(standardHeaderMap,
-                    headers,
-                    inputPayload,
-                    targetURL,
-                    params,
-                    status,
-                    httpMethod);
+    private Response performRESTCall(final String targetURL,
+                                     final Headers headers,
+                                     final Map<String, String> params,
+                                     final HttpMethod httpMethod,
+                                     final HttpStatus status,
+                                     final String inputPayload) {
+        return shouldExecute(
+                headers,
+                inputPayload,
+                targetURL,
+                params,
+                status,
+                httpMethod);
+    }
+
+    private static final Headers convertHeaderMapToWireMockHeaders(final Map<String, String> headerMap) {
+
+        List<Header> listOfHeaders = new ArrayList<>();
+        headerMap.forEach((key, value) -> {
+            Header header = new Header(key, value);
+            listOfHeaders.add(header);
+        });
+        Headers headers = new Headers(listOfHeaders);
+        return headers;
     }
 }
