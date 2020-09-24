30edfba29b36a881d6db8bd8a66f41d2a7ca581c
SachinNaik <sachin.naik@hmcts.net>
2020-09-24 13:35:40 +0100

    HMIS-409 Fixing Sonar issues for Smoke Tests

--- src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/hearings/HearingApiSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/hearings/HearingApiSmokeTest.java
@@ -8,7 +8,6 @@ import uk.gov.hmcts.futurehearings.hmi.smoke.SmokeTest;
 
 import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,14 +17,14 @@ import org.springframework.test.context.ActiveProfiles;
 @Slf4j
 @SpringBootTest(classes = {Application.class})
 @ActiveProfiles("smoke")
-public class HearingApiSmokeTest extends SmokeTest {
+ class HearingApiSmokeTest extends SmokeTest {
 
     @Value("${hearingsApiRootContext}")
     private String hearingsApiRootContext;
 
     @Test
     @DisplayName("Smoke Test to Test the Endpoint for the Hearing Root Context")
-    public void testSuccessfulHearingApiGet() {
+     void testSuccessfulHearingApiGet() {
 
                 Response response = given()
                 .headers(headersAsMap)
