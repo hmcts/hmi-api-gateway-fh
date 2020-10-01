2bf9529d5f6a5b30c4bde6b3d0998d02ddb9ae0e
git-sabs <69256391+git-sabs@users.noreply.github.com>
2020-10-01 19:08:19 +0100

    Added the unit tests (#127)

---
+++ src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/testsuites/PUT_resources_location_UnitTests.java
@@ -0,0 +1,272 @@
+package uk.gov.hmcts.futurehearings.hmi.unit.testing.testsuites;
+
+import static io.restassured.RestAssured.given;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.HearingsResponseVerifier.thenValidateResponseForAdditionalParam;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.ResourcesResponseVerifier.thenValidateResponseForInvalidResource;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.ResourcesResponseVerifier.thenValidateResponseForInvalidSubscriptionKeyHeader;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.ResourcesResponseVerifier.thenValidateResponseForMissingOrInvalidAcceptHeader;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.ResourcesResponseVerifier.thenValidateResponseForMissingOrInvalidContentTypeHeader;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.ResourcesResponseVerifier.thenValidateResponseForMissingOrInvalidHeader;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.ResourcesResponseVerifier.thenValidateResponseForMissingSubscriptionKeyHeader;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.ResourcesResponseVerifier.thenValidateResponseForUpdate;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.TestUtilities.readFileContents;
+
+import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.futurehearings.hmi.Application;
+import uk.gov.hmcts.futurehearings.hmi.unit.testing.util.TestReporter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@SpringBootTest(classes = {Application.class})
+@ActiveProfiles("test")
+@ExtendWith(TestReporter.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DisplayName("PUT /resources/location - Update Location Resource")
+public class PUT_resources_location_UnitTests {
+    public static final String CORRECT_UPDATE_LOCATION_RESOURCE_PAYLOAD = "requests/correct-update-location-resource-payload.json";
+    @Value("${targetInstance}")
+    private String targetInstance;
+
+    @Value("${targetSubscriptionKey}")
+    private String targetSubscriptionKey;
+
+    @Value("${resourcesApiRootContext}")
+    private String resourcesApiRootContext;
+
+    private final Map<String, Object> headersAsMap = new HashMap<>();
+    private final Map<String, String> paramsAsMap = new HashMap<>();
+
+    @BeforeEach
+    public void initialiseValues() {
+
+        headersAsMap.put("Ocp-Apim-Subscription-Key", targetSubscriptionKey);
+        headersAsMap.put("Content-Type", "application/json");
+        headersAsMap.put("Accept", "application/json");
+        headersAsMap.put("Source-System", "CFT");
+        headersAsMap.put("Destination-System", "S&L");
+        headersAsMap.put("Request-Created-At", "2018-01-29 20:36:01Z");
+        headersAsMap.put("Request-Processed-At", "2018-02-29 20:36:01Z");
+        headersAsMap.put("Request-Type", "THEFT");
+
+        paramsAsMap.put("sessionIdCaseHQ", "CASE1234");
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Test for Invalid Resource")
+    public void testUpdateLocationResourceForInvalidResource() throws IOException {
+        final String input = givenAPayload(CORRECT_UPDATE_LOCATION_RESOURCE_PAYLOAD);
+        final Response response = whenUpdateLocationResourceIsInvokedForInvalidResource(input);
+        thenValidateResponseForInvalidResource(response);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Test for missing ContentType header")
+    public void testUpdateLocationResourceWithMissingContentTypeHeader() throws IOException {
+        headersAsMap.remove("Content-Type");
+        final String input = givenAPayload(CORRECT_UPDATE_LOCATION_RESOURCE_PAYLOAD);
+        final Response response = whenUpdateLocationResourceIsInvokedWithMissingOrInvalidHeader(input);
+        thenValidateResponseForMissingOrInvalidContentTypeHeader(response);
+    }
+    @Test
+    @Order(3)
+    @DisplayName("Test for invalid ContentType header")
+    public void testUpdateLocationResourceWithInvalidContentTypeHeader() throws IOException {
+        headersAsMap.remove("Content-Type");
+        headersAsMap.put("Content-Type", "application/xml");
+        final String input = givenAPayload(CORRECT_UPDATE_LOCATION_RESOURCE_PAYLOAD);
+        final Response response = whenUpdateLocationResourceIsInvokedWithMissingOrInvalidHeader(input);
+        thenValidateResponseForMissingOrInvalidContentTypeHeader(response);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Test for missing Accept header")
+    public void testUpdateLocationResourceWithMissingAcceptHeader() throws IOException {
+        headersAsMap.remove("Accept");
+        final String input = givenAPayload(CORRECT_UPDATE_LOCATION_RESOURCE_PAYLOAD);
+        final Response response = whenUpdateLocationResourceIsInvokedWithMissingOrInvalidHeader(input);
+        thenValidateResponseForMissingOrInvalidAcceptHeader(response);
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Test for invalid Accept header")
+    public void testUpdateLocationResourceWithInvalidAcceptHeader() throws IOException {
+        headersAsMap.remove("Accept");
+        headersAsMap.put("Accept", "application/jsonxml");
+        final String input = givenAPayload(CORRECT_UPDATE_LOCATION_RESOURCE_PAYLOAD);
+        final Response response = whenUpdateLocationResourceIsInvokedWithMissingOrInvalidHeader(input);
+        thenValidateResponseForMissingOrInvalidAcceptHeader(response);
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Test for missing OcpSubKey")
+    public void testUpdateLocationResourceRequestWithMissingOcpSubKey() throws IOException {
+        headersAsMap.remove("Ocp-Apim-Subscription-Key");
+        final String input = givenAPayload(CORRECT_UPDATE_LOCATION_RESOURCE_PAYLOAD);
+        final Response response = whenUpdateHearingIsInvokedWithMissingOrInvalidOcSubKey(input);
+        thenValidateResponseForMissingSubscriptionKeyHeader(response);
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("Test for invalid Ocp-Apim-Subscription-Key header")
+    public void testUpdateLocationResourceRequestWithInvalidOcpSubKey()throws IOException {
+        headersAsMap.remove("Ocp-Apim-Subscription-Key");
+        headersAsMap.put("Ocp-Apim-Subscription-Key","invalidocpsubkey");
+        final String input = givenAPayload(CORRECT_UPDATE_LOCATION_RESOURCE_PAYLOAD);
+        final Response response = whenUpdateHearingIsInvokedWithMissingOrInvalidOcSubKey(input);
+        thenValidateResponseForInvalidSubscriptionKeyHeader(response);
+    }
+
+    @Order(8)
+    @ParameterizedTest(name = "Test for missing {0} header")
+    @ValueSource(strings = {"Source-System","Destination-System","Request-Created-At","Request-Processed-At","Request-Type"})
+    void testUpdateLocationResourceWithMissingHeader(String iteration) throws IOException {
+        headersAsMap.remove(iteration);
+        final String input = givenAPayload(CORRECT_UPDATE_LOCATION_RESOURCE_PAYLOAD);
+        final Response response = whenUpdateLocationResourceIsInvokedWithMissingOrInvalidHeader(input);
+        thenValidateResponseForMissingOrInvalidHeader(response, iteration);
+    }
+
+    @Order(9)
+    @ParameterizedTest(name = "Test for invalid {0} header")
+    @ValueSource(strings = {"Source-System","Destination-System","Request-Created-At","Request-Processed-At","Request-Type"})
+    void testUpdateLocationResourceWithInvalidHeader(String iteration) throws IOException {
+        headersAsMap.remove(iteration);
+        headersAsMap.put(iteration, "A");
+        final String input = givenAPayload(CORRECT_UPDATE_LOCATION_RESOURCE_PAYLOAD);
+        final Response response = whenUpdateLocationResourceIsInvokedWithMissingOrInvalidHeader(input);
+        thenValidateResponseForMissingOrInvalidHeader(response, iteration);
+    }
+
+    @Test
+    @Order(10)
+    @DisplayName("Test for Additional Parameter")
+    void testUpdateLocationResourceRequestWithAdditionalParam() throws IOException {
+        paramsAsMap.put("Invalid-Param","Value");
+        final String input = givenAPayload(CORRECT_UPDATE_LOCATION_RESOURCE_PAYLOAD);
+        final Response response = whenUpdateLocationResourceIsInvokedWithAdditionalParameter(input);
+        thenValidateResponseForAdditionalParam(response);
+    }
+
+    @Test
+    @Order(11)
+    @DisplayName("Test for correct Headers and Params")
+    public void testUpdateLocationResourceRequestWithCorrectHeadersAndParams() throws IOException {
+
+        final String input = givenAPayload(CORRECT_UPDATE_LOCATION_RESOURCE_PAYLOAD);
+        final Response response = whenUpdateLocationResourceIsInvokedWithCorrectHeadersAndParams(input);
+        thenValidateResponseForUpdate(response);
+    }
+
+    @Test
+    @Order(12)
+    @DisplayName("Test for correct Request")
+    public void testUpdateLocationResourceRequestWithCorrectHeadersAndNoParams() throws IOException {
+
+        final String input = givenAPayload(CORRECT_UPDATE_LOCATION_RESOURCE_PAYLOAD);
+        final Response response = whenUpdateLocationResourceIsInvokedWithCorrectHeadersAndNoParams(input);
+        thenValidateResponseForUpdate(response);
+    }
+
+    private String givenAPayload(final String path) throws IOException {
+        return readFileContents(path);
+    }
+
+    private Response whenUpdateLocationResourceIsInvokedWithMissingOrInvalidHeader(final String input) {
+        return updateLocationResourceResponseForAMissingOrInvalidHeader(resourcesApiRootContext + "/location", headersAsMap, targetInstance, paramsAsMap, input);
+    }
+
+    private Response whenUpdateHearingIsInvokedWithMissingOrInvalidOcSubKey(final String input) {
+        return updateLocationResourceResponseForAMissingOrInvalidOcpSubKey(resourcesApiRootContext + "/location", headersAsMap, targetInstance, paramsAsMap, input);
+    }
+
+    private Response whenUpdateLocationResourceIsInvokedForInvalidResource(final String input) {
+        return updateLocationResourceResponseForInvalidResource(resourcesApiRootContext+"/locationput", headersAsMap, targetInstance, paramsAsMap, input);
+    }
+
+    private Response whenUpdateLocationResourceIsInvokedWithCorrectHeadersAndParams(final String input) {
+        return updateLocationResourceResponseForCorrectHeadersAndParams(resourcesApiRootContext+ "/location", headersAsMap,  paramsAsMap, targetInstance, input);
+    }
+
+    private Response whenUpdateLocationResourceIsInvokedWithAdditionalParameter(final String input) {
+        return updateLocationResourceResponseForCorrectHeadersAndParams(resourcesApiRootContext+ "/location", headersAsMap,  paramsAsMap, targetInstance, input);
+    }
+
+    private Response whenUpdateLocationResourceIsInvokedWithCorrectHeadersAndNoParams(final String input) {
+        return updateLocationResourceResponseForCorrectHeadersAndNoParams(resourcesApiRootContext+ "/location", headersAsMap, targetInstance, input);
+    }
+
+    private Response updateLocationResourceResponseForInvalidResource(final String api, final Map<String, Object> headersAsMap, final String basePath, final Map<String, String> paramsAsMap, final String payloadBody) {
+
+        return given()
+                .queryParams(paramsAsMap)
+                .body(payloadBody)
+                .headers(headersAsMap)
+                .baseUri(basePath)
+                .basePath(api)
+                .when().put().then().extract().response();
+    }
+
+    private Response updateLocationResourceResponseForCorrectHeadersAndParams(final String api, final Map<String, Object> headersAsMap, final Map<String, String> paramsAsMap, final String basePath,  final String payloadBody) {
+
+        return given()
+                .queryParams(paramsAsMap)
+                .body(payloadBody)
+                .headers(headersAsMap)
+                .baseUri(basePath)
+                .basePath(api)
+                .when().put().then().extract().response();
+    }
+
+
+    private Response updateLocationResourceResponseForCorrectHeadersAndNoParams(final String api, final Map<String, Object> headersAsMap, final String basePath,  final String payloadBody) {
+
+        return given()
+                .headers(headersAsMap)
+                .body(payloadBody)
+                .baseUri(basePath)
+                .basePath(api)
+                .when().put().then().extract().response();
+    }
+
+    private Response updateLocationResourceResponseForAMissingOrInvalidHeader(final String api, final Map<String, Object> headersAsMap, final String basePath, final Map<String, String> paramsAsMap, final String payloadBody) {
+        return given()
+                .queryParams(paramsAsMap)
+                .body(payloadBody)
+                .headers(headersAsMap)
+                .baseUri(basePath)
+                .basePath(api)
+                .when().put().then().extract().response();
+    }
+
+    private Response updateLocationResourceResponseForAMissingOrInvalidOcpSubKey(final String api, final Map<String, Object> headersAsMap, final String basePath, final Map<String, String> paramsAsMap, final String payloadBody) {
+        return  given()
+                .queryParams(paramsAsMap)
+                .body(payloadBody)
+                .headers(headersAsMap)
+                .baseUri(basePath)
+                .basePath(api)
+                .when().put().then().extract().response();
+    }
+}
