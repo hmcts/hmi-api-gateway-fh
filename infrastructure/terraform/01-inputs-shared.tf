1714e790fa86d2a54984dac3973690537d4d4b52
karanotts <nottinghamkara@gmail.com>
2020-10-08 11:02:14 +0100

    skip network

--- infrastructure/terraform/01-inputs-shared.tf
+++ infrastructure/terraform/01-inputs-shared.tf
@@ -6,5 +6,5 @@ variable "tags" {}
 variable "repo_branch" {}
 variable "virtual_network_type" {
   description = "Network type: None / External / Internal"
-  default     = null
+  default     = "None"
 }
