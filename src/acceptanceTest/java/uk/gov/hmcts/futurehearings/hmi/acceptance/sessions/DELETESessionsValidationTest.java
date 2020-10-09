87d9560a94042c62185fbc5d51708fa9fe31b911
Pettedson John <pettedson.john@hmcts.net>
2020-10-09 11:34:07 +0100

    Added all of the Sessions PUT and DELETE Tests which were eecuted and fixed.....

--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/sessions/DELETESessionsValidationTest.java
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/sessions/DELETESessionsValidationTest.java
@@ -2,20 +2,30 @@ package uk.gov.hmcts.futurehearings.hmi.acceptance.sessions;
 
 import static uk.gov.hmcts.futurehearings.hmi.acceptance.common.helper.CommonHeaderHelper.createStandardPayloadHeader;
 
+import uk.gov.hmcts.futurehearings.hmi.Application;
 import uk.gov.hmcts.futurehearings.hmi.acceptance.common.delegate.CommonDelegate;
 import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.error.HMICommonErrorVerifier;
 import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.success.HMICommonSuccessVerifier;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 
-public class DELETESessionsValidationTest extends SessionsValidationTest {
+@Slf4j
+@SpringBootTest(classes = {Application.class})
+@ActiveProfiles("acceptance")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SuppressWarnings("java:S2187")
+class DELETESessionsValidationTest extends SessionsValidationTest {
 
     @Qualifier("CommonDelegate")
     @Autowired(required = true)
@@ -30,7 +40,7 @@ public class DELETESessionsValidationTest extends SessionsValidationTest {
         sessions_idRootContext = String.format(sessions_idRootContext,"12345");
         this.setRelativeURL(sessions_idRootContext);
         this.setHttpMethod(HttpMethod.DELETE);
-        this.setInputPayloadFileName("delete-sessions-standard.json");
+        this.setInputPayloadFileName("delete-sessions-request-valid.json");
         this.setHttpSucessStatus(HttpStatus.OK);
         this.setRelativeURLForNotFound(this.getRelativeURL().replace("sessions","session"));
         this.setHmiSuccessVerifier(new HMICommonSuccessVerifier());
