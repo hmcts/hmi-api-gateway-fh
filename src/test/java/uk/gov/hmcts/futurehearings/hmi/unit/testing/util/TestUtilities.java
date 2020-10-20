29c44729d9664ce801d6daf7b380dde09913072a
mjotichmcts <70706159+mjotichmcts@users.noreply.github.com>
2020-10-20 13:45:08 +0100

    Hmis 295 develop (#153)
    * HMIS-295: JWT policy added to /secure endpoint
    
    * fixed the typo
    
    * Hmis 461 o auth unit tests (#154)
    
    * Added initial tests
    
    * Updated tests
    
    * Removed log
    
    Co-authored-by: Milan Jotic <milan.jotic@beo-tech.com>
    Co-authored-by: git-sabs <69256391+git-sabs@users.noreply.github.com>
    Co-authored-by: Sabarish <Sabarish.Palaniswamy@hmcts.net>

--- src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/util/TestUtilities.java
+++ src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/util/TestUtilities.java
@@ -1,5 +1,8 @@
 package uk.gov.hmcts.futurehearings.hmi.unit.testing.util;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.expect;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
@@ -12,6 +15,24 @@ public class TestUtilities {
     public static String readFileContents (final String path) throws IOException {
         final File file = ResourceUtils.getFile("classpath:" + path);
         return new String(Files.readAllBytes(Paths.get(file.toURI())));
+    }
+
+    public static String getToken(String grantType,String clientID,String clientSecret,String tokenURL, String scope)
+    {
+        final String body = String.format("grant_type=%s&client_id=%s&client_secret=%s&scope=%s", grantType, clientID, clientSecret, scope);
+        Response response = expect().that().statusCode(200).
+                            given()
+                                .body(body)
+                                .contentType(ContentType.URLENC)
+                                .baseUri(tokenURL)
+                            .when()
+                                .post()
+                            .then()
+                                .extract()
+                                .response();
+
+        return response.jsonPath().getString("access_token");
+
     }
 
 }
