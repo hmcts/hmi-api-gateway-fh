4c1f3c55ae4744b9ae909520b26cfd5391be42eb
Pettedson John <pettedson.john@hmcts.net>
2020-10-23 18:40:28 +0100

    Added all of the Debug Changes for the Wiremock mocks to be set.....

--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/common/mock/CommonStubFactory.java
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/common/mock/CommonStubFactory.java
@@ -3,23 +3,22 @@ package uk.gov.hmcts.futurehearings.hmi.acceptance.common.mock;
 import static uk.gov.hmcts.futurehearings.hmi.acceptance.common.RestClientTemplate.shouldExecute;
 
 import java.util.List;
-
 import io.restassured.http.Header;
 import io.restassured.http.Headers;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
+@Slf4j
 public class CommonStubFactory {
 
     private CommonStubFactory() {
-
     }
 
     public static void resetMocks (String requestURL) {
 
         Header header = new Header("Content-Type", "application/json");
         Headers headers = new Headers(List.of(header));
-
         shouldExecute(headers,
                 "{}",
                 requestURL,
@@ -28,7 +27,10 @@ public class CommonStubFactory {
                 HttpMethod.POST);
     }
 
-    public static  void uploadCommonMocks (String requestURL, String payload) {
+    public static void uploadCommonMocks (String requestURL, String payload) {
+        
+        log.debug("The value of the RequestURL "+requestURL);
+        log.debug("The value of the body " +payload);
 
         Header header = new Header("Content-Type", "application/json");
         Headers headers = new Headers(List.of(header));
