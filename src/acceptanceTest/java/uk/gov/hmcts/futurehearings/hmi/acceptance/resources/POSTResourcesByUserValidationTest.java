24b02c1147be6c0356074b1f03719e8e1ccfb495
SachinNaik <sachin.naik@hmcts.net>
2020-10-23 16:21:27 +0100

    HMIS-328 Adding acceptance tests for POST Resource as User and Location

---
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/resources/POSTResourcesByUserValidationTest.java
@@ -0,0 +1,50 @@
+package uk.gov.hmcts.futurehearings.hmi.acceptance.resources;
+
+import uk.gov.hmcts.futurehearings.hmi.Application;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.common.delegate.CommonDelegate;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.error.HMICommonErrorVerifier;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.success.HMICommonSuccessVerifier;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@Slf4j
+@SpringBootTest(classes = {Application.class})
+@ActiveProfiles("acceptance")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SelectClasses(POSTResourcesByUserValidationTest.class)
+@IncludeTags("POST")
+class POSTResourcesByUserValidationTest extends ResourceValidationTest {
+
+    @Qualifier("CommonDelegate")
+    @Autowired(required = true)
+    public CommonDelegate commonDelegate;
+
+    @Value("${resourcesByUserRootContext}")
+    private String resourcesByUserRootContext;
+
+    private HttpMethod httpMethod;
+
+    @BeforeAll
+    public void initialiseValues() {
+        super.initialiseValues();
+        this.setRelativeURL(resourcesByUserRootContext);
+        this.setHttpMethod(HttpMethod.POST);
+        this.setInputPayloadFileName("post-user-as-resource-request-valid.json");
+        this.setHttpSucessStatus(HttpStatus.OK);
+        this.setRelativeURLForNotFound(this.getRelativeURL().replace("resources/user","resource/user"));
+        this.setHmiSuccessVerifier(new HMICommonSuccessVerifier());
+        this.setHmiErrorVerifier(new HMICommonErrorVerifier());
+    }
+}
