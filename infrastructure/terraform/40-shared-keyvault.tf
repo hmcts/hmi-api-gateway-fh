4c88e43f676d301ce991fb8055f21cbeaf248c3e
karanotts <nottinghamkara@gmail.com>
2020-10-28 09:43:13 +0000

    add api id to kv

--- infrastructure/terraform/40-shared-keyvault.tf
+++ infrastructure/terraform/40-shared-keyvault.tf
@@ -3,12 +3,12 @@ data "azurerm_key_vault" "infra_key_vault" {
   resource_group_name = "hmi-sharedservices-${var.environment}-rg"
 }
 
-resource "azurerm_key_vault_access_policy" "shared_kv_premissions" {
-  key_vault_id            = data.azurerm_key_vault.infra_key_vault.id
-  tenant_id               = azurerm_key_vault.hmi_apim_kv.tenant_id
-  object_id               = azurerm_api_management.hmi_apim.id
-  certificate_permissions = var.certificate_permissions
-  key_permissions         = var.key_permissions
-  secret_permissions      = var.secret_permissions
-  storage_permissions     = var.storage_permissions
-}
+# resource "azurerm_key_vault_access_policy" "shared_kv_premissions" {
+#   key_vault_id            = data.azurerm_key_vault.infra_key_vault.id
+#   tenant_id               = azurerm_key_vault.hmi_apim_kv.tenant_id
+#   object_id               = azurerm_api_management.hmi_apim.id
+#   certificate_permissions = var.certificate_permissions
+#   key_permissions         = var.key_permissions
+#   secret_permissions      = var.secret_permissions
+#   storage_permissions     = var.storage_permissions
+# }
