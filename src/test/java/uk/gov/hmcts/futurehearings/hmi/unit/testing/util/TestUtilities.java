e9a1970bfc258a7c674f39c10cbb74d234f36da9
Pettedson John <pettedson.john@hmcts.net>
2020-10-28 12:15:06 +0000

    Added all the relevant code for the commonising of the AuthToken Generator......

--- src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/util/TestUtilities.java
+++ src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/util/TestUtilities.java
@@ -2,8 +2,11 @@ package uk.gov.hmcts.futurehearings.hmi.unit.testing.util;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+
 import static io.restassured.RestAssured.expect;
+
 import org.springframework.util.ResourceUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,24 +14,23 @@ import java.nio.file.Paths;
 
 public class TestUtilities {
 
-    public static String readFileContents (final String path) throws IOException {
+    public static String readFileContents(final String path) throws IOException {
         final File file = ResourceUtils.getFile("classpath:" + path);
         return new String(Files.readAllBytes(Paths.get(file.toURI())));
     }
 
-    public static String getToken(String grantType,String clientID,String clientSecret,String tokenURL, String scope)
-    {
+    public static String getToken(String grantType, String clientID, String clientSecret, String tokenURL, String scope) {
         final String body = String.format("grant_type=%s&client_id=%s&client_secret=%s&scope=%s", grantType, clientID, clientSecret, scope);
         Response response = expect().that().statusCode(200).
-                            given()
-                                .body(body)
-                                .contentType(ContentType.URLENC)
-                                .baseUri(tokenURL)
-                            .when()
-                                .post()
-                            .then()
-                                .extract()
-                                .response();
+                given()
+                .body(body)
+                .contentType(ContentType.URLENC)
+                .baseUri(tokenURL)
+                .when()
+                .post()
+                .then()
+                .extract()
+                .response();
 
         return response.jsonPath().getString("access_token");
 
