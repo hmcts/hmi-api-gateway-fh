2fb4b13d59dbd79364f8a0f4dbc972e0463d41bd
Pettedson John <pettedson.john@hmcts.net>
2020-10-28 12:15:06 +0000

    Added all the relevant code for the commonising of the AuthToken Generator......

---
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/common/security/OAuthTokenGenerator.java
@@ -0,0 +1,47 @@
+package uk.gov.hmcts.futurehearings.hmi.acceptance.common.security;
+
+import static io.restassured.RestAssured.expect;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+
+@Slf4j
+public class OAuthTokenGenerator {
+
+    public static final void generateOAuthToken(final String token_apiURL,
+                                                final String token_apiTenantId,
+                                                final String grantType,
+                                                final String clientID,
+                                                final String clientSecret,
+                                                final String scope,
+                                                final HttpStatus httpStatus) throws Exception {
+
+        String full_token_apiURL = String.format(token_apiURL, token_apiTenantId);
+        final String bodyForToken = String.format("grant_type=%s&client_id=%s&client_secret=%s&scope=%s",
+                grantType, clientID, clientSecret, scope);
+
+        callTokenGeneratorEndpoint(bodyForToken, httpStatus, full_token_apiURL);
+    }
+
+    public static final void callTokenGeneratorEndpoint(final String bodyForToken,
+                                                         final HttpStatus badRequest,
+                                                         final String full_token_apiURL) {
+
+        log.debug("The value of the Body : " + bodyForToken);
+        log.debug("The value of the Target URL : " + full_token_apiURL);
+
+        Response response = expect().that().statusCode(badRequest.value())
+                .given()
+                .body(bodyForToken)
+                .contentType(ContentType.URLENC)
+                .baseUri(full_token_apiURL)
+                .when()
+                .post()
+                .then()
+                .extract()
+                .response();
+        log.debug(response.prettyPrint());
+    }
+}
