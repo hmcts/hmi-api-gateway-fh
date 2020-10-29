58384ed424587b71fa5a429567e401724b372e5a
karanotts <nottinghamkara@gmail.com>
2020-10-29 12:06:09 +0000

    use az keyvault cmd

--- script/apply_custom_domain.ps1
+++ script/apply_custom_domain.ps1
@@ -27,7 +27,8 @@ if ($apim.ProxyCustomHostnameConfiguration.Hostname -notcontains $proxy.Hostname
     $apim.ProxyCustomHostnameConfiguration = $proxy
     Write-Host "Applying Custom Domain configuration..." $proxy.Hostname -ForegroundColor Yellow
     $apimObjectId = ($apim | Select-Object -Expand Identity | Select -ExpandProperty PrincipalId)
-    Set-AzKeyVaultAccessPolicy -VaultName $KeyVaultName -ObjectId $apimObjectId -PermissionsToSecrets Get,List -Force -Confirm:$false
+    az keyvault set-policy --name $KeyVaultName --object-id $apimObjectId --secret-permissions get, list
+    # Set-AzKeyVaultAccessPolicy -VaultName $KeyVaultName -ObjectId $apimObjectId -PermissionsToSecrets Get,List -Force -Confirm:$false
     Set-AzApiManagement -InputObject $apim -Verbose -Debug
     Write-Host "Custom domain successfully applied..."
     Write-Host "Listing custom domains..."
