ed0847eec0856184bdfd4df2b4718d8110c482b3
karanotts <nottinghamkara@gmail.com>
2020-10-29 09:13:59 +0000

    script output

--- script/apply_custom_domain.ps1
+++ script/apply_custom_domain.ps1
@@ -26,11 +26,9 @@ $apim = Get-AzApiManagement -ResourceGroupName $ResourceGroupName
 if ($apim.ProxyCustomHostnameConfiguration.Hostname -notcontains $proxy.Hostname) {
     $apim.ProxyCustomHostnameConfiguration = $proxy
     Write-Host "Applying Custom Domain configuration..." $proxy.Hostname -ForegroundColor Yellow
-    try {
     $apimObjectId = ($apim | Select-Object -Expand Identity | Select -ExpandProperty PrincipalId)
     Set-AzKeyVaultAccessPolicy -VaultName $KeyVaultName -ResourceGroupName $ResourceGroupName -ObjectId $apimObjectId -PermissionsToSecrets Get,List -Verbose -Debug
     Set-AzApiManagement -InputObject $apim -Verbose -Debug
-    } catch {}
     Write-Host "Custom domain successfully applied..."
     Write-Host "Listing custom domains..."
     $apim = Get-AzApiManagement -ResourceGroupName $ResourceGroupName
