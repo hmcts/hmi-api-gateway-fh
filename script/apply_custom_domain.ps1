ab1ef9f289cf1c3534b8c518b1f317debc1479fb
karanotts <nottinghamkara@gmail.com>
2020-11-01 08:56:48 +0000

    test bash

---
+++ script/apply_custom_domain.ps1
@@ -0,0 +1,48 @@
+[CmdletBinding()]
+Param (
+[Parameter(Mandatory=$true)]
+[string] $Hostname,
+
+[Parameter(Mandatory=$true)]
+[string] $HostnameType,
+
+[Parameter(Mandatory=$true)]
+[string] $KeyVaultName,
+
+[Parameter(Mandatory=$true)]
+[string] $ResourceGroupName
+)
+
+$tenant = (Get-AzTenant).Id
+$Credential = Get-Credential
+Connect-AzAccount -Credential $Credential -Tenant $tenant -ServicePrincipal
+
+if (!(Get-Module -Name Az.ApiManagement)){
+    Write-Host "Installing Az.ApiManagement Module..." -ForegroundColor Yellow
+    Install-Module -Name Az.ApiManagement -Force -Verbose
+    Write-Host "Az.ApiManagement Module successfully installed..."
+    } else {
+        Write-Host "Az.ApiManagement already installed, skipping" -ForegroundColor Green
+    }
+$KeyVaultId = "https://$KeyVaultName.vault.azure.net/secrets/apim-hostname-certificate"
+$proxy = (New-AzApiManagementCustomHostnameConfiguration -Hostname $Hostname -HostnameType $HostnameType -KeyVaultId $KeyVaultId -DefaultSslBinding)
+$apim = (Get-AzApiManagement -ResourceGroupName $ResourceGroupName)
+if ($apim.ProxyCustomHostnameConfiguration.Hostname -notcontains $proxy.Hostname) {
+    $apim.ProxyCustomHostnameConfiguration = $proxy
+    Write-Host "Applying Custom Domain configuration..." $proxy.Hostname -ForegroundColor Yellow
+    $apimObjectId = ($apim | Select-Object -Expand Identity | Select -ExpandProperty PrincipalId)
+    Set-AzKeyVaultAccessPolicy -VaultName $KeyVaultName -ObjectId $apimObjectId -PermissionsToSecrets Get,List -Confirm:$false
+    Set-AzApiManagement -InputObject $apim -SystemAssignedIdentity -Confirm:$false
+    Write-Host "Custom domain successfully applied..."
+    Write-Host "Listing custom domains..."
+    $apim = Get-AzApiManagement -ResourceGroupName $ResourceGroupName
+    foreach ($_ in $apim.ProxyCustomHostnameConfiguration.Hostname) {
+        Write-Host $_
+    }
+} else {
+    Write-Host "Listing custom domains..."
+    foreach ($_ in $apim.ProxyCustomHostnameConfiguration.Hostname) {
+        Write-Host $_
+    }
+    Write-Host "Custom domain already applied, skipping..." -ForegroundColor Green
+}
