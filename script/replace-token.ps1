f3e75d1d5bd142451d96f7858ca76749f529bd33
Milan Jotic <milan.jotic@beo-tech.com>
2020-11-11 13:07:15 +0000

    ps for token replacement

---
+++ script/replace-token.ps1
@@ -0,0 +1,12 @@
+$RootPath = Split-Path -Parent $MyInvocation.MyCommand.Path | Split-Path
+Write-Host 'Replacing tokens  in Policy files. Please wait...'
+Get-ChildItem $RootPath -Filter '*-policy.xml' -Recurse | 
+Foreach-Object {
+    $file = Get-Content $_.FullName
+    $containsWord = $file | %{$_ -match "#{hostName}#"}
+    if ($containsWord -contains $true) {
+      (Get-Content $_.FullName) -replace  '#{hostName}#','http://milan.org' | Set-Content $_.FullName
+        Write-Host "** Updated Token in File:  " $_.FullName
+    } 
+}
+Write-Host 'Replacement complete.'
