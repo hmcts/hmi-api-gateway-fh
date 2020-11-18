2bf5dd592a6baecd2834f521f3bb906ba6ed6f87
Pettedson John <pettedson.john@hmcts.net>
2020-11-18 15:23:52 +0000

    Initial cut and delivery of the snlacceptance tests....

---
+++ src/snlacceptanceTest/java/uk/gov/hmcts/futurehearings/snl/acceptance/hearings/HearingValidationTest.java
@@ -0,0 +1,66 @@
+package uk.gov.hmcts.futurehearings.snl.acceptance.hearings;
+
+import static io.restassured.config.EncoderConfig.encoderConfig;
+import static uk.gov.hmcts.futurehearings.snl.acceptance.common.helper.CommonHeaderHelper.createStandardPayloadHeader;
+import static uk.gov.hmcts.futurehearings.snl.acceptance.common.security.OAuthTokenGenerator.generateOAuthToken;
+
+import uk.gov.hmcts.futurehearings.hmi.Application;
+import uk.gov.hmcts.futurehearings.snl.acceptance.common.test.HMICommonHeaderTest;
+
+import io.restassured.RestAssured;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@Slf4j
+@SpringBootTest(classes = {Application.class})
+@ActiveProfiles("snlacceptance")
+public abstract class HearingValidationTest extends HMICommonHeaderTest {
+
+    @Value("${targetInstance}")
+    private String targetInstance;
+
+    @Value("${targetSubscriptionKey}")
+    private String targetSubscriptionKey;
+
+    @Value("${token_apiURL}")
+    private String token_apiURL;
+
+    @Value("${token_apiTenantId}")
+    private String token_apiTenantId;
+
+    @Value("${grantType}")
+    private String grantType;
+
+    @Value("${clientID}")
+    private String clientID;
+
+    @Value("${clientSecret}")
+    private String clientSecret;
+
+    @Value("${scope}")
+    private String scope;
+
+    @BeforeAll
+    public void initialiseValues() throws Exception {
+        RestAssured.baseURI = targetInstance;
+        RestAssured.useRelaxedHTTPSValidation();
+        this.setApiSubscriptionKey(targetSubscriptionKey);
+        RestAssured.config = RestAssured.config()
+                .encoderConfig(encoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false));
+        this.setInputFileDirectory("hearings");
+        String authorizationToken = generateOAuthToken (token_apiURL,
+                token_apiTenantId,
+                grantType, clientID,
+                clientSecret,
+                scope,
+                HttpStatus.OK);
+        this.setAuthorizationToken(authorizationToken);
+    }
+}
