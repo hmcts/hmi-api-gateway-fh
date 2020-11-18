a2f07e0f8228a14f23703800aa4729c3232a1b46
Pettedson John <pettedson.john@hmcts.net>
2020-11-18 15:23:52 +0000

    Initial cut and delivery of the snlacceptance tests....

---
+++ src/snlacceptanceTest/java/uk/gov/hmcts/futurehearings/snl/acceptance/common/verify/success/HMISuccessVerifier.java
@@ -0,0 +1,6 @@
+package uk.gov.hmcts.futurehearings.snl.acceptance.common.verify.success;
+
+import uk.gov.hmcts.futurehearings.snl.acceptance.common.verify.HMIVerifier;
+
+public interface HMISuccessVerifier extends HMIVerifier {
+}
