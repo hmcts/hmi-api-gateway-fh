ebd01c3897497404fd0d666324016a3101ca20b0
463952 <subroto.mukherjee@HMCTS.NET>
2020-11-23 16:22:28 +0000

    HMIS-602: Fixing get sessions policy

--- src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/util/SessionsResponseVerifier.java
+++ src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/util/SessionsResponseVerifier.java
@@ -232,7 +232,7 @@ public class SessionsResponseVerifier {
             //assertEquals(2, responseMap.size());
             assertEquals(400, response.getStatusCode(),"Status Code Validation:");
             getObjStep().pass("Got the expected status code: 400");
-            assertEquals("Invalid query parameter/s in the request URL.", responseMap.get(("message")),"Status Code Message Validation:");
+            assertEquals("Invalid query parameter/s in the request URL", responseMap.get(("message")),"Status Code Message Validation:");
             getObjStep().pass("Got the expected message: " + responseMap.get(("message")));
         }
         catch (AssertionError e){
