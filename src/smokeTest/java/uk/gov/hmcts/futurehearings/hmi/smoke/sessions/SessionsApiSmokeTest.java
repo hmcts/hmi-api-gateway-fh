b9678f60b6b0615fa4f31d2df6b9035e72a894a9
SachinNaik <sachin.naik@hmcts.net>
2020-11-24 14:34:58 +0000

    Fixing Smoke Tests

--- src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/sessions/SessionsApiSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/sessions/SessionsApiSmokeTest.java
@@ -1,22 +1,13 @@
 package uk.gov.hmcts.futurehearings.hmi.smoke.sessions;
 
-import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import uk.gov.hmcts.futurehearings.hmi.Application;
 import uk.gov.hmcts.futurehearings.hmi.smoke.common.test.SmokeTest;
 
-import io.restassured.response.Response;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 
 @Slf4j
