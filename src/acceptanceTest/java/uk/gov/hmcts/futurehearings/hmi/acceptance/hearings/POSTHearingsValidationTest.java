6a063b96e06d32c67b36c03bff68e8e25c5d9bc5
Pettedson John <pettedson.john@hmcts.net>
2020-11-26 14:16:03 +0000

    Initial Version of the branch for Testing....

--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/hearings/POSTHearingsValidationTest.java
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/hearings/POSTHearingsValidationTest.java
@@ -72,10 +72,6 @@ class POSTHearingsValidationTest extends HearingValidationTest {
                 getHmiSuccessVerifier(),"The request was received successfully.",null);
     }
 
-    //@Test
-    //@Tag("sbox")
-    //@Tag("test")
-    //@DisplayName("Successfully validated response with all the header values from a Wiremock Instance")
     @ParameterizedTest(name = "Testing against the Emulator for Error Responses that come from the Case HQ System")
     @CsvSource(value = {"EMULATOR,400,1000,Invalid LOV Value","EMULATOR,400,1003,mandatory value missing","EMULATOR,400,1004,schema validation failure"}, nullValues = "NIL")
     void test_successful_response_from_the_emulator_stub(final String destinationSystem,
