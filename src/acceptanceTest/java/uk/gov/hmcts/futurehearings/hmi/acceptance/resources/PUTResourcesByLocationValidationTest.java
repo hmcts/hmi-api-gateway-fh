073a3bdb722346ac5db52821a9ca3188f9bfdf83
SachinNaik <sachin.naik@hmcts.net>
2020-11-30 11:19:21 +0000

    Adding tests for Resources Mock emulator

--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/resources/PUTResourcesByLocationValidationTest.java
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/resources/PUTResourcesByLocationValidationTest.java
@@ -1,17 +1,12 @@
 package uk.gov.hmcts.futurehearings.hmi.acceptance.resources;
 
-import static uk.gov.hmcts.futurehearings.hmi.acceptance.common.helper.CommonHeaderHelper.createHeaderWithEmulatorValues;
-
 import uk.gov.hmcts.futurehearings.hmi.Application;
-import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.error.CaseHQCommonErrorVerifier;
 import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.error.HMICommonErrorVerifier;
 import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.success.HMICommonSuccessVerifier;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
@@ -45,32 +40,5 @@ public class PUTResourcesByLocationValidationTest extends ResourceValidationTest
         this.setRelativeURLForNotFound(this.getRelativeURL().replace("location","locatio"));
         this.setHmiSuccessVerifier(new HMICommonSuccessVerifier());
         this.setHmiErrorVerifier(new HMICommonErrorVerifier());
-    }
-
-    @ParameterizedTest(name = "Testing against the Emulator for Error Responses that come from the Case HQ System")
-    @CsvSource(value = {"EMULATOR,400,1000,Invalid LOV Value","EMULATOR,400,1003,mandatory value missing","EMULATOR,400,1004,schema validation failure"}, nullValues = "NIL")
-    void test_successful_response_from_the_emulator_stub(final String destinationSystem,
-                                                         final String returnHttpCode,
-                                                         final String returnErrorCode,
-                                                         final String returnDescription) throws Exception {
-
-        final HttpStatus httpStatus =
-                returnHttpCode.equalsIgnoreCase("400") ? HttpStatus.BAD_REQUEST : HttpStatus.NOT_ACCEPTABLE;
-        commonDelegate.test_expected_response_for_supplied_header(getApiSubscriptionKey(),
-                getAuthorizationToken(),
-                getRelativeURL(), "put-resource-by-location-request-valid.json",
-                createHeaderWithEmulatorValues(getApiSubscriptionKey(),
-                        destinationSystem,
-                        returnHttpCode,
-                        returnErrorCode,
-                        returnDescription),
-                null,
-                getUrlParams(),
-                getHttpMethod(),
-                httpStatus,
-                getInputFileDirectory(),
-                new CaseHQCommonErrorVerifier(),
-                returnDescription,
-                null);
     }
 }
