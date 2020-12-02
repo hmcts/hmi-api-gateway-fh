3fce12cc32d623ad2bc22b774d4ddac14a314263
mjotichmcts <70706159+mjotichmcts@users.noreply.github.com>
2020-12-02 12:59:25 +0000

    Hmis 605 dev (#238)
    * Initial Version of the branch for Testing....
    
    * Update api-op-get-sessions-policy.xml
    
    * Defect Fix
    
    * Adding tests for Resources mock emulator
    
    * Adding the GETSessionsValidationTest post final testing.....
    
    * Added all tne Mocking Refactored Tests for Hearings and Listings and new Tests for Sessionsand Schedules
    
    * removed validation of Request-Processed-At
    
    * added OAuth prior to call
    
    * Adding tests for Resources Mock emulator
    
    * Initial Version of the branch for Testing....
    
    * Update api-op-get-sessions-policy.xml
    
    * Defect Fix
    
    * Adding tests for Resources mock emulator
    
    * Adding the GETSessionsValidationTest post final testing.....
    
    * Added all tne Mocking Refactored Tests for Hearings and Listings and new Tests for Sessionsand Schedules
    
    * Adding tests for Resources Mock emulator
    
    * added send-request
    
    * added OAuth for S&L
    
    * updated for McGirr oAuth
    
    * updated with actual S&L values
    
    * added tokens for S&L oAuth
    
    * added tokens for S&L oAuth
    
    * restored Request-Processed-At check
    
    Co-authored-by: Pettedson John <pettedson.john@hmcts.net>
    Co-authored-by: Subroto Mukherjee <53174127+subrotomukherjee@users.noreply.github.com>
    Co-authored-by: pats-john <13101669+pats-john@users.noreply.github.com>
    Co-authored-by: 463952 <subroto.mukherjee@HMCTS.NET>
    Co-authored-by: SachinNaik <sachin.naik@hmcts.net>
    Co-authored-by: Milan Jotic <milan.jotic@beo-tech.com>

--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/sessions/GETSessionsByIDValidationTest.java
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/sessions/GETSessionsByIDValidationTest.java
@@ -1,12 +1,18 @@
 package uk.gov.hmcts.futurehearings.hmi.acceptance.sessions;
 
+import static uk.gov.hmcts.futurehearings.hmi.acceptance.common.helper.CommonHeaderHelper.createHeaderWithEmulatorValues;
+import static uk.gov.hmcts.futurehearings.hmi.acceptance.common.helper.QueryParamsHelper.buildQueryParams;
+
 import uk.gov.hmcts.futurehearings.hmi.Application;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.error.CaseHQCommonErrorVerifier;
 import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.error.HMICommonErrorVerifier;
 import uk.gov.hmcts.futurehearings.hmi.acceptance.sessions.verify.GETSessionByIdValidationVerifier;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
@@ -40,5 +46,33 @@ public class GETSessionsByIDValidationTest extends SessionsValidationTest {
         this.setHmiSuccessVerifier(new GETSessionByIdValidationVerifier());
         this.setHmiErrorVerifier(new HMICommonErrorVerifier());
     }
+
+    @ParameterizedTest(name = "Testing against the Emulator for Error Responses that come from the Case HQ System")
+    @CsvSource(value = {"EMULATOR,400,1000,mandatory value missing", "EMULATOR,400,1003,Bad LOV value"}, nullValues = "NIL")
+    void test_successful_response_from_the_emulator_stub(final String destinationSystem,
+                                                         final String returnHttpCode,
+                                                         final String returnErrorCode,
+                                                         final String returnDescription) throws Exception {
+
+        final HttpStatus httpStatus =
+                returnHttpCode.equalsIgnoreCase("400") ? HttpStatus.BAD_REQUEST : HttpStatus.NOT_ACCEPTABLE;
+        commonDelegate.test_expected_response_for_supplied_header(getApiSubscriptionKey(),
+                getAuthorizationToken(),
+                getRelativeURL(), getInputPayloadFileName(),
+                createHeaderWithEmulatorValues(getApiSubscriptionKey(),
+                        destinationSystem,
+                        returnHttpCode,
+                        returnErrorCode,
+                        returnDescription),
+                null,
+                getUrlParams(),
+                getHttpMethod(),
+                httpStatus,
+                getInputFileDirectory(),
+                new CaseHQCommonErrorVerifier(),
+                returnDescription,
+                null);
+    }
+
 
 }
