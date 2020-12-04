ce1d9be330a5d2da230c6174679de6c99aaa6b60
463952 <subroto.mukherjee@HMCTS.NET>
2020-12-04 16:16:48 +0000

    HMIS-584: Adding new end point for EHR/ELINKS

--- src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/util/ListingsResponseVerifier.java
+++ src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/util/ListingsResponseVerifier.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.futurehearings.hmi.unit.testing.util;
 
 import io.restassured.response.Response;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,6 +23,70 @@ public class ListingsResponseVerifier {
             getObjStep().pass("Got the expected status code: 404");
             assertEquals("Resource not found", responseMap.get(("message")),"Status Code Message Validation:");
             getObjStep().pass("Got the expected message: " + responseMap.get(("message")));
+        }
+        catch (AssertionError e){
+            getObjStep().fail("Exception in "+e.getMessage());
+            throw e;
+        }
+        catch (Exception e){
+            getObjStep().fail("Exception: "+e.getClass());
+            throw e;
+        }
+    }
+
+    public static void  thenValidateResponseForGetPeopleById(Response response){
+        try{
+            Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
+            assertEquals(200, response.getStatusCode(),"Status Code Validation:");
+            getObjStep().pass("Got the expected status code: 200");
+        }
+        catch (AssertionError e){
+            getObjStep().fail("Exception in "+e.getMessage());
+            throw e;
+        }
+        catch (Exception e){
+            getObjStep().fail("Exception: "+e.getClass());
+            throw e;
+        }
+    }
+
+    public static void  thenValidateResponseForGetPeopleByIdWithInvalidHeader(Response response){
+        try{
+            Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
+            assertEquals(400, response.getStatusCode(),"Status Code Validation:");
+            getObjStep().pass("Got the expected status code: 400");
+        }
+        catch (AssertionError e){
+            getObjStep().fail("Exception in "+e.getMessage());
+            throw e;
+        }
+        catch (Exception e){
+            getObjStep().fail("Exception: "+e.getClass());
+            throw e;
+        }
+    }
+
+    public static void  thenValidateResponseForGetPeopleByParams(Response response){
+        try{
+            List<Map<String, String>> responseMap = response.getBody().jsonPath().getList("$");
+            assertEquals(200, response.getStatusCode(),"Status Code Validation:");
+            getObjStep().pass("Got the expected status code: 200");
+        }
+        catch (AssertionError e){
+            getObjStep().fail("Exception in "+e.getMessage());
+            throw e;
+        }
+        catch (Exception e){
+            getObjStep().fail("Exception: "+e.getClass());
+            throw e;
+        }
+    }
+
+    public static void  thenValidateInvalidResponseForGetPeopleByParams(Response response){
+        try{
+            Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
+            assertEquals(400, response.getStatusCode(),"Status Code Validation:");
+            getObjStep().pass("Got the expected status code: 400");
         }
         catch (AssertionError e){
             getObjStep().fail("Exception in "+e.getMessage());
