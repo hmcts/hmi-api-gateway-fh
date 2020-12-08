d0fc5cf7ac3d9f422c9c53f2a2eee4aaeda0a1fc
SachinNaik <sachin.naik@hmcts.net>
2020-12-08 15:41:08 +0000

    Adding functional test for PUT People and incorporating review comments

--- src/functionalTest/java/uk/gov/hmcts/futurehearings/hmi/functional/people/steps/PeopleSteps.java
+++ src/functionalTest/java/uk/gov/hmcts/futurehearings/hmi/functional/people/steps/PeopleSteps.java
@@ -1,15 +1,18 @@
-package uk.gov.hmcts.futurehearings.hmi.functional.People.steps;
+package uk.gov.hmcts.futurehearings.hmi.functional.people.steps;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static uk.gov.hmcts.futurehearings.hmi.functional.People.PeopleLookUpResponseProcess.getPeopleId;
+import static uk.gov.hmcts.futurehearings.hmi.functional.common.rest.RestClientTemplate.callRestEndpointWithPayload;
 import static uk.gov.hmcts.futurehearings.hmi.functional.common.rest.RestClientTemplate.callRestEndpointWithQueryParams;
+import static uk.gov.hmcts.futurehearings.hmi.functional.people.PeopleLookUpResponseProcess.getPeopleId;
 
 import java.util.Map;
 import java.util.Objects;
 
 import io.restassured.response.Response;
 import net.thucydides.core.annotations.Step;
+import org.json.JSONObject;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
 public class PeopleSteps {
@@ -31,7 +34,7 @@ public class PeopleSteps {
     }
 
     @Step("User makes a request to Get List of People for relevant search parameters on the People API")
-    public void performGetByPeopleId(final String apiURL,
+    public void performGetByPeopleId(final String apiURL, final String peopleId,
                                        final Map<String, Object> headersAsMap,
                                        final String authorizationToken,
                                        final Map<String, String> queryParameters) throws Exception {
@@ -41,5 +44,20 @@ public class PeopleSteps {
                 authorizationToken,
                 queryParameters, HttpStatus.OK);
         assertEquals(HttpStatus.OK.value(),response.getStatusCode());
+
+        JSONObject peopleObj = new JSONObject(response.body().asString());
+        assertEquals(peopleId ,peopleObj.getString("id"));
+    }
+
+    @Step("User makes a request to Update People (PUT in the People API)")
+    public void performPeopleUpdate(final String apiURL,
+                                                      final Map<String, Object> headersAsMap,
+                                                      final String authorizationToken,
+                                                      final String body) {
+        Response response = callRestEndpointWithPayload(apiURL,
+                headersAsMap,
+                authorizationToken,
+                body, HttpMethod.PUT,HttpStatus.NO_CONTENT);
+        assertEquals(HttpStatus.NO_CONTENT.value(),response.getStatusCode());
     }
 }
