92dd6dd188784852fbe17806f9951341f4f74646
Pettedson John <pettedson.john@hmcts.net>
2020-12-15 15:36:07 +0000

    Adding all the acceptance tests to the QA branch for the Purpose of Acceptance testing...

---
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/people/GETPeopleByIDValidationTest.java
@@ -0,0 +1,46 @@
+package uk.gov.hmcts.futurehearings.hmi.acceptance.people;
+
+import uk.gov.hmcts.futurehearings.hmi.Application;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.error.HMICommonErrorVerifier;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.success.HMICommonSuccessVerifier;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.resources.ResourceValidationTest;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.resources.verify.GETResourceByIDValidationVerifier;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+
+@Slf4j
+@SpringBootTest(classes = {Application.class})
+@ActiveProfiles("acceptance")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SuppressWarnings("java:S2187")
+class GETPeopleByIDValidationTest extends PeopleValidationTest {
+
+    @Value("${targetInstance}")
+    private String targetInstance;
+
+    @Value("${targetSubscriptionKey}")
+    private String targetSubscriptionKey;
+
+    @Value("${people_idRootContext}")
+    private String people_idRootContext;
+
+    @BeforeAll
+    public void initialiseValues() throws Exception {
+        super.initialiseValues();
+        people_idRootContext = String.format(people_idRootContext,"12345");
+        this.setRelativeURL(people_idRootContext);
+        this.setHttpMethod(HttpMethod.GET);
+        this.setHttpSucessStatus(HttpStatus.OK);
+        this.setRelativeURLForNotFound(this.getRelativeURL().replace("people","peopl"));
+        this.setHmiSuccessVerifier(new HMICommonSuccessVerifier());
+        this.setHmiErrorVerifier(new HMICommonErrorVerifier());
+    }
+}
