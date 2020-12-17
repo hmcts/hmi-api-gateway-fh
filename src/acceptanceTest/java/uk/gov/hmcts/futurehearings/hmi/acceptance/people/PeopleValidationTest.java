c332c3ab95a72a3da855bd56899e9cc807c33ef1
mjotichmcts <70706159+mjotichmcts@users.noreply.github.com>
2020-12-17 10:27:35 +0000

    Hmis 639 dev (#261)
    * Added the First PeopleBy Test to the Acceptance job in the Repo......
    
    * Undoing a previous commit put in this branch by mistake
    
    * HMIS-584: Adding new end point for EHR/ELINKS
    
    * HMIS-584: Adding token for validation header to elinks
    
    * HMIS-584: Adding token for validation header to elinks
    
    * change and to or
    
    * rm test stages
    
    * change condition
    
    * change condition
    
    * rm tets dependencies
    
    * Adding acceptance tests for Post Video Hearing
    
    * Incorporating review comments
    
    * Undoing a previous commit put in this branch by mistake
    
    * HMIS-584: Adding token for validation header to elinks
    
    * HMIS-584: Fixing policy variables json files
    
    * HMIS-635: Adding POST end point for Video Hearing
    
    * HMIS-584: Fixing issues of json compliance and naming conventions
    
    * Removed the Untested GETPeopleByIDValidationTest.java from the Feature branch
    
    * Adding the Smoke Tests for Testing purposes...
    
    * corrected message when updated_since is missing
    
    * Hmis 588 dev (#247)
    
    * new policy
    
    * added issuer for jwt valdiation
    
    * added update-people elinks
    
    * corrected file name
    
    * initial tests
    
    * updated tests
    
    * sample payload
    
    * refactored
    
    * set profile to test
    
    * added update-people
    
    * added Elinks variables
    
    * added Elinks tokens
    
    * fixed var names
    
    * added missing '#'
    
    * fixed second url
    
    Co-authored-by: Milan Jotic <milan.jotic@beo-tech.com>
    Co-authored-by: 463952 <subroto.mukherjee@HMCTS.NET>
    
    * Adding all the acceptance tests to the QA branch for the Purpose of Acceptance testing...
    
    * Adding the Smoke Tests for Testing purposes...
    
    * Adding all the acceptance tests to the QA branch for the Purpose of Acceptance testing...
    
    * HMIS-635: Fixing issues of json compliance and naming conventions
    
    * Added all the working acceptance tests to the Repo post testing...
    
    * Fixing test assertion
    
    * Adding all the Tested Functional tests to the QA branch ....
    
    * disable trace for prod
    
    * copy typo
    
    * tracing
    
    * Revert "Prod stage"
    
    * Added a comment to elaborate and clarify the Source-System and Destination-System positive tests...
    
    * fixed a typo
    
    * updated tests with type value
    
    * removed valid values
    
    Co-authored-by: Pettedson John <pettedson.john@hmcts.net>
    Co-authored-by: 463952 <subroto.mukherjee@HMCTS.NET>
    Co-authored-by: karanotts <nottinghamkara@gmail.com>
    Co-authored-by: SachinNaik <sachin.naik@hmcts.net>
    Co-authored-by: Subroto Mukherjee <53174127+subrotomukherjee@users.noreply.github.com>
    Co-authored-by: Milan Jotic <milan.jotic@beo-tech.com>
    Co-authored-by: pats-john <13101669+pats-john@users.noreply.github.com>
    Co-authored-by: Kara Nottingham <45759232+karanotts@users.noreply.github.com>

---
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/people/PeopleValidationTest.java
@@ -0,0 +1,62 @@
+package uk.gov.hmcts.futurehearings.hmi.acceptance.people;
+
+import static io.restassured.config.EncoderConfig.encoderConfig;
+import static uk.gov.hmcts.futurehearings.hmi.acceptance.common.security.OAuthTokenGenerator.generateOAuthToken;
+
+import uk.gov.hmcts.futurehearings.hmi.Application;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.common.test.HMICommonHeaderTest;
+
+import io.restassured.RestAssured;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+
+@Slf4j
+@SpringBootTest(classes = {Application.class})
+@ActiveProfiles("acceptance")
+public abstract class PeopleValidationTest extends HMICommonHeaderTest {
+
+    @Value("${targetInstance}")
+    private String targetInstance;
+
+    @Value("${targetSubscriptionKey}")
+    private String targetSubscriptionKey;
+
+    @Value("${token_apiURL}")
+    private String token_apiURL;
+
+    @Value("${token_apiTenantId}")
+    private String token_apiTenantId;
+
+    @Value("${grantType}")
+    private String grantType;
+
+    @Value("${clientID}")
+    private String clientID;
+
+    @Value("${clientSecret}")
+    private String clientSecret;
+
+    @Value("${scope}")
+    private String scope;
+
+    @BeforeAll
+    public void initialiseValues() throws Exception {
+        RestAssured.baseURI = targetInstance;
+        RestAssured.useRelaxedHTTPSValidation();
+        this.setApiSubscriptionKey(targetSubscriptionKey);
+        RestAssured.config = RestAssured.config()
+                .encoderConfig(encoderConfig().appendDefaultContentCharsetToContentTypeIfUndefined(false));
+        this.setInputFileDirectory("people");
+        String authorizationToken = generateOAuthToken (token_apiURL,
+                token_apiTenantId,
+                grantType, clientID,
+                clientSecret,
+                scope,
+                HttpStatus.OK);
+        this.setAuthorizationToken(authorizationToken);
+    }
+}
