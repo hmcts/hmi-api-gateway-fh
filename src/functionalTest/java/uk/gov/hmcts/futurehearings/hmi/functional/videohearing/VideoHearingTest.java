32c970a63c3a486ad1d06af4c0ec9ce029dadec1
SachinNaik <sachin.naik@hmcts.net>
2020-12-18 16:18:16 +0000

    Adding functional test for Video Hearing

---
+++ src/functionalTest/java/uk/gov/hmcts/futurehearings/hmi/functional/videohearing/VideoHearingTest.java
@@ -0,0 +1,79 @@
+package uk.gov.hmcts.futurehearings.hmi.functional.videohearing;
+
+import static uk.gov.hmcts.futurehearings.hmi.functional.common.TestingUtils.readFileContents;
+
+import uk.gov.hmcts.futurehearings.hmi.Application;
+import uk.gov.hmcts.futurehearings.hmi.functional.common.test.FunctionalTest;
+import uk.gov.hmcts.futurehearings.hmi.functional.videohearing.steps.VideoHearingSteps;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import lombok.extern.slf4j.Slf4j;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import net.thucydides.core.annotations.Narrative;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@Slf4j
+@RunWith(SpringIntegrationSerenityRunner.class)
+@Narrative(text = {"In order to test that the Video Hearing Functionality is working properly",
+        "As a tester",
+        "I want to be able to execute the tests for People API"})
+@SpringBootTest(classes = {Application.class})
+@ActiveProfiles("functional")
+@SuppressWarnings("java:S2699")
+public class VideoHearingTest extends FunctionalTest {
+
+    @Value("${videohearingsRootContext}")
+    protected String videohearingsRootContext;
+
+    @Value("${videohearings_idRootContext}")
+    protected String videohearings_idRootContext;
+
+    @Steps
+    VideoHearingSteps videoHearingSteps;
+
+    @Before
+    public void initialiseValues() throws Exception {
+        super.initialiseValues();
+    }
+
+
+    @Test
+    public void testVideoHearing() throws Exception {
+
+        log.debug("In the testSuccessfulPostVideoHearing() method");
+        final String username = String.format("abc" + new Random().nextInt(999999));
+        final String payloadForVideoHearing =
+                String.format(readFileContents("uk/gov/hmcts/futurehearings/hmi/functional/direct-listing/input/POST-video-hearing-request.json"), username);
+
+        //Make Post call for video hearing with username
+        videoHearingSteps.makePostForVideoHearing(videohearingsRootContext,
+                headersAsMap,
+                authorizationToken,
+                payloadForVideoHearing);
+
+        //Make Get call for video hearing with username as query param
+        Map<String, String> queryParameters = new HashMap<String, String>();
+        queryParameters.put("username", username);
+
+        final String hearingId = videoHearingSteps.performVideoHearingGetByUsername(videohearingsRootContext,
+                headersAsMap,
+                authorizationToken,
+                queryParameters);
+
+        //Make Get call for video hearing with hearingId as path param
+        videohearings_idRootContext = String.format(videohearings_idRootContext, hearingId);
+        videoHearingSteps.performVideoHearingGetByHearingId(videohearings_idRootContext, hearingId,
+                headersAsMap,
+                authorizationToken,
+                null);
+    }
+}
