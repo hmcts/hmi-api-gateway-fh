66e7b4df8497aecfdf0f4a62f397867178c6bfd5
Milan Jotic <milan.jotic@beo-tech.com>
2020-12-18 17:30:13 +0000

    corrected profile

--- src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/testsuites/GET_sessions_UnitTests.java
+++ src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/testsuites/GET_sessions_UnitTests.java
@@ -21,7 +21,7 @@ import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.SessionsResponse
 
 @Slf4j
 @SpringBootTest(classes = {Application.class})
-@ActiveProfiles("local")
+@ActiveProfiles("test")
 @ExtendWith(TestReporter.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
