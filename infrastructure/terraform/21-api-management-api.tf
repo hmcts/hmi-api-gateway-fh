10a163261e9cc45c882d3e0102f6e99d3c7cdd74
463952 <subroto.mukherjee@HMCTS.NET>
2021-02-04 14:14:27 +0000

    HMIS-722: New api for health check url.

--- infrastructure/terraform/21-api-management-api.tf
+++ infrastructure/terraform/21-api-management-api.tf
@@ -16,3 +16,21 @@ resource "azurerm_api_management_api" "hmi_apim_api" {
     content_value  = var.open_api_spec_content_value
   }
 }
+resource "azurerm_api_management_api" "hmi_apim_api" {
+  name                = "${var.prefix}-${var.product}-api-health"
+  resource_group_name = azurerm_resource_group.hmi_apim_rg.name
+  api_management_name = azurerm_api_management.hmi_apim.name
+  revision            = var.revision
+  display_name        = "${var.prefix}-${var.product}-api-health"
+  path                = var.prefix
+  protocols           = var.protocols
+
+  service_url = var.service_url
+
+  subscription_required = false
+
+  import {
+    content_format = var.open_api_spec_content_format
+    content_value  = var.open_api_spec_content_value
+  }
+}
