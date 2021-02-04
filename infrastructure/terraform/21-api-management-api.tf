a1b28b14388e90c03b958d0fc0e157d745b84c71
Milan Jotic <milan.jotic@beo-tech.com>
2021-02-04 18:18:30 +0000

    fixed formatting

--- infrastructure/terraform/21-api-management-api.tf
+++ infrastructure/terraform/21-api-management-api.tf
@@ -7,7 +7,7 @@ resource "azurerm_api_management_api" "hmi_apim_api" {
   path                = var.prefix
   protocols           = var.protocols
 
-  service_url         = var.service_url
+  service_url = var.service_url
 
   subscription_required = false
 
@@ -18,14 +18,14 @@ resource "azurerm_api_management_api" "hmi_apim_api" {
 }
 
 resource "azurerm_api_management_api" "hmi_apim_api_health" {
-  name                = "${var.prefix}-${var.product}-api-health"
-  resource_group_name = azurerm_resource_group.hmi_apim_rg.name
-  api_management_name = azurerm_api_management.hmi_apim.name
-  revision            = var.revision
-  display_name        = "${var.prefix}-${var.product}-api-health"
-  path                = "health"
-  protocols           = var.protocols
-  service_url         = var.service_url
+  name                  = "${var.prefix}-${var.product}-api-health"
+  resource_group_name   = azurerm_resource_group.hmi_apim_rg.name
+  api_management_name   = azurerm_api_management.hmi_apim.name
+  revision              = var.revision
+  display_name          = "${var.prefix}-${var.product}-api-health"
+  path                  = "health"
+  protocols             = var.protocols
+  service_url           = var.service_url
   subscription_required = false
 
   import {
