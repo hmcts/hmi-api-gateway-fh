957eeb6cc9271f42b93fcd2939a8532090c0c81b
SachinNaik <sachin.naik@hmcts.net>
2021-02-05 18:32:42 +0000

    HMIS-750 Adding acceptance and functional tests for PUT and DELETE VH Hearing

---
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/videohearings/PUTVideoHearingsValidationTest.java
@@ -0,0 +1,70 @@
+package uk.gov.hmcts.futurehearings.hmi.acceptance.videohearings;
+
+import static uk.gov.hmcts.futurehearings.hmi.acceptance.common.helper.CommonHeaderHelper.createStandardPayloadHeader;
+
+import uk.gov.hmcts.futurehearings.hmi.Application;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.common.delegate.CommonDelegate;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.error.HMICommonErrorVerifier;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.success.HMICommonSuccessVerifier;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@Slf4j
+@SpringBootTest(classes = {Application.class})
+@ActiveProfiles("acceptance")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SelectClasses(PUTVideoHearingsValidationTest.class)
+@IncludeTags("Post")
+class PUTVideoHearingsValidationTest extends VideoHearingValidationTest {
+
+    @Qualifier("CommonDelegate")
+    @Autowired(required = true)
+    private CommonDelegate commonDelegate;
+
+    @Value("${videohearings_idRootContext}")
+    private String videohearings_idRootContext;
+
+    @BeforeAll
+    public void initialiseValues() throws Exception {
+        super.initialiseValues();
+        this.setRelativeURL(videohearings_idRootContext);
+        this.setHttpMethod(HttpMethod.PUT);
+        this.setInputPayloadFileName("put-video-hearing-request.json");
+        this.setHttpSucessStatus(HttpStatus.OK);
+        this.setRelativeURLForNotFound(this.getRelativeURL().replace("video-hearing","video-hearings"));
+        this.setHmiSuccessVerifier(new HMICommonSuccessVerifier());
+        this.setHmiErrorVerifier(new HMICommonErrorVerifier());
+    }
+
+    //This test is for a Standard Header but a Payload for Non JSON Type is to be tested.
+    //Confirmed by Product Owner that this should be a Success Scenario.
+    @Test
+    @DisplayName("Successfully validated response with an xml payload")
+    void test_successful_response_for_test_xml_body() throws Exception {
+
+        commonDelegate.test_expected_response_for_supplied_header(
+                getAuthorizationToken(),
+                getRelativeURL(), "sample-xml-payload.xml",
+                createStandardPayloadHeader(),
+                null,
+                getUrlParams(),
+                getHttpMethod(),
+                this.getHttpSucessStatus(),
+                "common",
+                getHmiSuccessVerifier(),"The request was received successfully.",null);
+    }
+}
