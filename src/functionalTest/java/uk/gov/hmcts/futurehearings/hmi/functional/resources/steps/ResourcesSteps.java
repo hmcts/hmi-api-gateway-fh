140025154af3f7479f4cfa0736d4dcb9c10bc0bf
Matt <matt@Matthews-MacBook-Pro-2.local>
2021-03-09 14:34:16 +0000

    removed user tests from functional tests

--- src/functionalTest/java/uk/gov/hmcts/futurehearings/hmi/functional/resources/steps/ResourcesSteps.java
+++ src/functionalTest/java/uk/gov/hmcts/futurehearings/hmi/functional/resources/steps/ResourcesSteps.java
@@ -14,41 +14,6 @@ import org.springframework.http.HttpStatus;
 @Slf4j
 public class ResourcesSteps {
 
-    private String resourcesUser;
-
-    @Step("User performs the creation of a user using the Resources API")
-    public void shouldCreateAnUser(final String apiURL,
-                                   final Map<String, Object> headersAsMap,
-                                   final String authorizationToken,
-                                   final String body) {
-
-        Response response = callRestEndpointWithPayload(apiURL,
-                headersAsMap,
-                authorizationToken,
-                body,
-                HttpMethod.POST,
-                HttpStatus.CREATED);
-        System.out.println(response.getBody().asString());
-        assertEquals(HttpStatus.CREATED.value(), response.getStatusCode());
-
-    }
-
-    @Step("User performs the amend of a user using the Resources API")
-    public void shouldUpdateAnUser(final String apiURL,
-                                   final Map<String, Object> headersAsMap,
-                                   final String authorizationToken,
-                                   final String body) {
-
-        Response response = callRestEndpointWithPayload(apiURL,
-                headersAsMap,
-                authorizationToken,
-                body,
-                HttpMethod.PUT,
-                HttpStatus.NO_CONTENT);
-        assertEquals(HttpStatus.NO_CONTENT.value(), response.getStatusCode());
-
-    }
-
     @Step("User performs the creation of a location using the Resources API")
     public void shouldCreateALocation(final String apiURL,
                                       final Map<String, Object> headersAsMap,
@@ -83,23 +48,6 @@ public class ResourcesSteps {
 
     }
 
-    @Step("User performs the creation of a user with invalid payload")
-    public void shouldCreateOrAmendUserWithInvalidPayload(final String apiURL,
-                                   final Map<String, Object> headersAsMap,
-                                   final String authorizationToken,
-                                   final HttpMethod httpMethod,
-                                   final String body) {
-
-        Response response = callRestEndpointWithPayload(apiURL,
-                headersAsMap,
-                authorizationToken,
-                body,
-                httpMethod,
-                HttpStatus.BAD_REQUEST);
-        assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatusCode());
-
-    }
-
     @Step("User performs the creation of a location using invalid payload")
     public void shouldCreateOrAmendLocationWithInvalidPayload(final String apiURL,
                                       final Map<String, Object> headersAsMap,
@@ -117,6 +65,4 @@ public class ResourcesSteps {
         assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatusCode());
 
     }
-
-
 }
