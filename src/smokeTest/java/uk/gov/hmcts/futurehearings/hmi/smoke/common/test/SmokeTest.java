502a02c9179e3d999a42588f7859847f4749e698
Matthew Summerbell <matt.summerbell@HMCTS.NET>
2021-03-11 09:12:57 +0000

    added repeated test to HmiApiGet

--- src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/common/test/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/common/test/SmokeTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static uk.gov.hmcts.futurehearings.hmi.smoke.common.header.factory.HeaderFactory.createStandardHMIHeader;
 import static uk.gov.hmcts.futurehearings.hmi.smoke.common.security.OAuthTokenGenerator.generateOAuthToken;
 
+import org.junit.jupiter.api.*;
 import uk.gov.hmcts.futurehearings.hmi.Application;
 
 import java.util.HashMap;
@@ -18,14 +19,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
@@ -111,7 +104,7 @@ public abstract class SmokeTest {
         log.debug("Test execution Class Completed: " + info.getTestClass().get().getName());
     }
 
-    @Test
+    @RepeatedTest(2)
     @DisplayName("Smoke Test to Test the Endpoint for the HMI Root Context")
     void testSuccessfulHmiApiGet() {
 
