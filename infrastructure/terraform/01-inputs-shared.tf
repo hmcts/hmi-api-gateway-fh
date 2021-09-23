0e75de72c50e732375aff3c6946cca376335b9fe
Christopher Pateman <christopher.pateman@version1.com>
2021-09-23 12:25:22 +0100

    Change file

--- infrastructure/terraform/01-inputs-shared.tf
+++ infrastructure/terraform/01-inputs-shared.tf
@@ -27,7 +27,6 @@ locals {
   common_tags   = module.ctags.common_tags
   env_long_name = var.environment == "sbox" ? "sandbox" : (var.environment == "stg" ? "staging" : var.environment)
 }
-
 module "ctags" {
   source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
   environment = var.environment
