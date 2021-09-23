ebaa66392adc7a5d5f594b238b4a007e4dea632d
Christopher Pateman <christopher.pateman@version1.com>
2021-09-23 16:06:12 +0100

    remove policy; add output

--- infrastructure/terraform/28-api-management-custom-domain.tf
+++ infrastructure/terraform/28-api-management-custom-domain.tf
@@ -1,17 +1,19 @@
-data "azurerm_key_vault" "kv" {
-  name                = "acmedtssds${var.environment}"
-  resource_group_name = "sds-platform-${var.environment}-rg"
-}
+
 locals {
   env_long_name = var.environment == "sbox" ? "sandbox" : var.environment == "stg" ? "staging" : var.environment
   host_name     = local.env_long_name == "prod" ? "hmi-apim.platform.hmcts.net" : "hmi-apim.${var.environment}.platform.hmcts.net"
   cert_name     = replace(local.host_name, ".", "-")
 }
+
+data "azurerm_key_vault" "kv" {
+  name                = "acmedtssds${var.environment}"
+  resource_group_name = "sds-platform-${var.environment}-rg"
+}
 data "azurerm_key_vault_certificate" "cert" {
   name         = local.cert_name
   key_vault_id = data.azurerm_key_vault.kv.id
 }
-
+/* 
 data "azurerm_api_management" "hmi_apim_svc" {
   name                = azurerm_api_management.hmi_apim.name
   resource_group_name = azurerm_resource_group.hmi_apim_rg.name
@@ -24,6 +26,13 @@ resource "azurerm_key_vault_access_policy" "policy" {
   secret_permissions      = ["Get", "Set", "List", "Delete"]
   certificate_permissions = []
   storage_permissions     = []
+} */
+
+output "kv_id"{
+  value = data.azurerm_key_vault.kv.id
+}
+output "cert_id"{
+  value = data.azurerm_key_vault_certificate.cert.secret_id
 }
 
 resource "azurerm_api_management_custom_domain" "custom_domain" {
