7733d5231d217959d644e156527e99554594eb41
Christopher Pateman <christopher.pateman@version1.com>
2021-09-24 09:32:34 +0100

    Merge branch 'master' into HMIS-921

--- infrastructure/terraform/00-init.tf
+++ infrastructure/terraform/00-init.tf
@@ -1,11 +1,7 @@
 terraform {
   backend "azurerm" {}
 
-<<<<<<< HEAD
-  required_version = ">= 0.14.0"
-=======
   required_version = ">= 1.0.4"
->>>>>>> master
   required_providers {
     azurerm = ">= 2.42.0"
   }
