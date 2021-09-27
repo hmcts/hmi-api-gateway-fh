f21f74aa933577236caee64210153d8148d36530
Christopher Pateman <christopher.pateman@version1.com>
2021-09-27 08:47:19 +0100

    Add policy

--- infrastructure/terraform/28-api-management-custom-domain.tf
+++ infrastructure/terraform/28-api-management-custom-domain.tf
@@ -23,6 +23,15 @@ resource "azurerm_role_assignment" "kv_access" {
   role_definition_name = "Key Vault Secrets User"
   principal_id         = data.azurerm_api_management.hmi_apim_svc.identity.0.principal_id
 }
+resource "azurerm_key_vault_access_policy" "policy" {
+  key_vault_id            = data.azurerm_key_vault.kv.id
+  tenant_id               = data.azurerm_client_config.current.tenant_id
+  object_id               = data.azurerm_api_management.hmi_apim_svc.identity.0.principal_id
+  key_permissions         = []
+  secret_permissions      = ["Get", "List"]
+  certificate_permissions = ["Get", "List"]
+  storage_permissions     = []
+}
 
 resource "azurerm_api_management_custom_domain" "custom_domain" {
   api_management_id = azurerm_api_management.hmi_apim.id
