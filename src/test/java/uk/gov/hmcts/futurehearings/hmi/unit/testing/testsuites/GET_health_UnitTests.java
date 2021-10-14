d8f236f38a498bdff088a247d1258ad71c30453a
Milan Jotic <milan.jotic@beo-tech.com>
2021-10-14 13:32:18 +0200

    added tests

---
+++ src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/testsuites/GET_health_UnitTests.java
@@ -0,0 +1,297 @@
+package uk.gov.hmcts.futurehearings.hmi.unit.testing.testsuites;
+
+import static io.restassured.RestAssured.given;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.SchedulesResponseVerifier.thenValidateResponseForInvalidSubscriptionKeyHeader;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.SchedulesResponseVerifier.thenValidateResponseForMissingOrInvalidAcceptHeader;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.SchedulesResponseVerifier.thenValidateResponseForMissingOrInvalidContentTypeHeader;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.SchedulesResponseVerifier.thenValidateResponseForMissingOrInvalidHeader;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.SchedulesResponseVerifier.thenValidateResponseForMissingSubscriptionKeyHeader;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.SchedulesResponseVerifier.thenValidateResponseForRetrieve;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.SchedulesResponseVerifier.thenValidateResponseForInvalidResource;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.SchedulesResponseVerifier.thenValidateResponseForMissingOrInvalidAccessToken;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import uk.gov.hmcts.futurehearings.hmi.Application;
+
+import uk.gov.hmcts.futurehearings.hmi.unit.testing.util.TestReporter;
+import uk.gov.hmcts.futurehearings.hmi.unit.testing.util.TestUtilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@Slf4j
+@SpringBootTest(classes = {Application.class})
+@ActiveProfiles("test")
+@ExtendWith(TestReporter.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("GET /schedules - Retrieve Hearing Schedules")
+@SuppressWarnings("java:S2699")
+class GET_schedules_UnitTests {
+
+    @Value("${targetInstance}")
+    private String targetInstance;
+
+    @Value("${targetSubscriptionKey}")
+    private String targetSubscriptionKey;
+
+    @Value("${schedulesApiRootContext}")
+    private String schedulesApiRootContext;
+
+    @Value("${destinationSystem}")
+    private String destinationSystem;
+
+    private final Map<String, Object> headersAsMap = new HashMap<>();
+    private final Map<String, String> paramsAsMap = new HashMap<>();
+
+    @Value("${tokenURL}")
+    private String tokenURL;
+
+    @Value("${clientID}")
+    private String clientID;
+
+    @Value("${clientSecret}")
+    private String clientSecret;
+
+    @Value("${scope}")
+    private String scope;
+
+    @Value("${grantType}")
+    private String grantType;
+
+    private static String accessToken;
+
+    @Value("${invalidTokenURL}")
+    private String invalidTokenURL;
+
+    @Value("${invalidScope}")
+    private String invalidScope;
+
+    @Value("${invalidClientID}")
+    private String invalidClientID;
+
+    @Value("${invalidClientSecret}")
+    private String invalidClientSecret;
+
+    @BeforeAll
+    void setToken(){
+        accessToken = TestUtilities.getToken(grantType, clientID, clientSecret, tokenURL, scope);
+    }
+
+    @BeforeEach
+    void initialiseValues() {
+
+        headersAsMap.put("Ocp-Apim-Trace", "true");
+        headersAsMap.put("Content-Type", "application/json");
+        headersAsMap.put("Source-System", "CFT");
+        headersAsMap.put("Destination-System", destinationSystem);
+        headersAsMap.put("Request-Type", "THEFT");
+        headersAsMap.put("Request-Created-At", "2018-01-29T20:36:01Z");
+        headersAsMap.put("Accept", "application/json");
+
+        paramsAsMap.put("hearing_date", "2018-02-29T20:36:01Z");
+        paramsAsMap.put("hearing_venue_id", "venueid");
+        paramsAsMap.put("hearing_room_id", "roomid");
+        paramsAsMap.put("hearing_session_id_casehq", "sessionid");
+        paramsAsMap.put("hearing_case_id_hmcts", "caseid");
+        paramsAsMap.put("hearing_id_casehq", "hearingid");
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Test for Invalid Resource")
+    void testRetrieveHearingSchedulesRequestForInvalidResource() {
+        final Response response = whenRetrieveHearingScheduleIsInvokedForInvalidResource();
+        thenValidateResponseForInvalidResource(response);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Test for missing ContentType header")
+    void testRetrieveHearingSchedulesRequestWithMissingContentTypeHeader() {
+        headersAsMap.remove("Content-Type");
+        final Response response = whenRetrieveHearingSchedulesIsInvokedWithMissingOrInvalidHeader();
+        thenValidateResponseForMissingOrInvalidContentTypeHeader(response);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Test for invalid ContentType header")
+    void testRetrieveHearingSchedulesRequestWithInvalidContentTypeHeader() {
+        headersAsMap.remove("Content-Type");
+        headersAsMap.put("Content-Type", "application/xml");
+
+        final Response response = whenRetrieveHearingSchedulesIsInvokedWithMissingOrInvalidHeader();
+        thenValidateResponseForMissingOrInvalidContentTypeHeader(response);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Test for missing Accept header")
+    void testRetrieveHearingSchedulesRequestWithMissingAcceptHeader() {
+        headersAsMap.remove("Accept");
+        final Response response = whenRetrieveHearingSchedulesIsInvokedWithMissingOrInvalidHeader();
+        thenValidateResponseForMissingOrInvalidAcceptHeader(response);
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Test for invalid Accept header")
+    void testDeleteHearingRequestWithInvalidAcceptHeader() {
+        headersAsMap.remove("Accept");
+        headersAsMap.put("Accept", "application/xml");
+
+        final Response response = whenRetrieveHearingSchedulesIsInvokedWithMissingOrInvalidHeader();
+        thenValidateResponseForMissingOrInvalidAcceptHeader(response);
+    }
+
+    @Order(6)
+    @ParameterizedTest(name = "Test for missing {0} header")
+    @ValueSource(strings = {"Source-System","Destination-System","Request-Created-At"})
+    void testRetrieveHearingSchedulesRequestWithMissingHeader(String iteration) {
+        headersAsMap.remove(iteration);
+
+        final Response response = whenRetrieveHearingSchedulesIsInvokedWithMissingOrInvalidHeader();
+        thenValidateResponseForMissingOrInvalidHeader(response, iteration);
+    }
+
+    @Order(7)
+    @ParameterizedTest(name = "Test for invalid {0} header")
+    @ValueSource(strings = {"Source-System","Destination-System","Request-Created-At"})
+    void testRetrieveHearingSchedulesRequestWithInvalidHeader(String iteration) {
+        headersAsMap.remove(iteration);
+        headersAsMap.put(iteration, "A");
+
+        final Response response = whenRetrieveHearingSchedulesIsInvokedWithMissingOrInvalidHeader();
+        thenValidateResponseForMissingOrInvalidHeader(response, iteration);
+    }
+
+    @Test
+    @Order(8)
+    @DisplayName("Test for Correct Headers and No Parameters")
+    void testRetrieveHearingSchedulesRequestWithCorrectRequestAndNoParams() {
+        final Response response = whenRetrieveHearingScheduleIsInvokedWithCorrectHeadersAndNoParams();
+        thenValidateResponseForRetrieve(response);
+    }
+
+    @Test
+    @Order(9)
+    @DisplayName("Test for Correct Headers and Parameters")
+    void testRetrieveHearingSchedulesRequestWithCorrectRequestAndAllParams() {
+        final Response response = whenRetrieveHearingScheduleIsInvokedWithCorrectHeadersAndAllParams();
+        thenValidateResponseForRetrieve(response);
+    }
+
+    @Test
+    @Order(10)
+    @DisplayName("Test for missing Access Token")
+    void testRetrieveHearingSchedulesRequestWithMissingAccessToken() {
+        final Response response = whenRetrieveHearingSchedulesIsInvokedWithMissingAccessToken();
+        thenValidateResponseForMissingOrInvalidAccessToken(response);
+    }
+
+    @Test
+    @Order(11)
+    @DisplayName("Test for invalid Access Token")
+    void testRetrieveHearingSchedulesRequestWithInvalidAccessToken()  {
+        accessToken = TestUtilities.getToken(grantType, invalidClientID, invalidClientSecret, invalidTokenURL, invalidScope);
+
+        final Response response = whenRetrieveHearingSchedulesIsInvokedWithMissingOrInvalidHeader();
+        thenValidateResponseForMissingOrInvalidAccessToken(response);
+    }
+
+    private Response whenRetrieveHearingScheduleIsInvokedForInvalidResource() {
+        return retrieveHearingSchedulesResponseForInvalidResource(schedulesApiRootContext +"get", headersAsMap, targetInstance);
+    }
+
+    private Response whenRetrieveHearingScheduleIsInvokedWithCorrectHeadersAndNoParams() {
+        return retrieveHearingSchedulesResponseForCorrectRequestAndNoParams(schedulesApiRootContext, headersAsMap, targetInstance);
+    }
+
+    private Response whenRetrieveHearingScheduleIsInvokedWithCorrectHeadersAndAllParams() {
+        return retrieveHearingSchedulesResponseForCorrectRequestAndAllParams(schedulesApiRootContext, headersAsMap, paramsAsMap, targetInstance);
+    }
+
+    private Response whenRetrieveHearingSchedulesIsInvokedWithMissingAccessToken() {
+        return retrieveHearingSchedulesResponseForMissingAccessToken(schedulesApiRootContext, headersAsMap, paramsAsMap, targetInstance);
+    }
+
+    private Response whenRetrieveHearingSchedulesIsInvokedWithMissingOrInvalidHeader() {
+        return retrieveHearingSchedulesResponseForMissingOrInvalidHeader(schedulesApiRootContext, headersAsMap, paramsAsMap, targetInstance);
+    }
+
+
+    private Response retrieveHearingSchedulesResponseForInvalidResource(final String api, final Map<String, Object> headersAsMap, final String basePath) {
+
+         return given()
+                 .auth()
+                 .oauth2(accessToken)
+                .headers(headersAsMap)
+                .baseUri(basePath)
+                .basePath(api)
+                .when().get().then().extract().response();
+    }
+
+    private Response retrieveHearingSchedulesResponseForCorrectRequestAndNoParams(final String api, final Map<String, Object> headersAsMap, final String basePath) {
+
+          return given()
+                  .auth()
+                  .oauth2(accessToken)
+                .headers(headersAsMap)
+                .baseUri(basePath)
+                .basePath(api)
+                .when().get().then().extract().response();
+    }
+
+    private Response retrieveHearingSchedulesResponseForCorrectRequestAndAllParams(final String api, final Map<String, Object> headersAsMap, final Map<String, String> paramsAsMap, final String basePath) {
+
+         return given()
+                 .auth()
+                 .oauth2(accessToken)
+                .queryParams(paramsAsMap)
+                .headers(headersAsMap)
+                .baseUri(basePath)
+                .basePath(api)
+                .when().get().then().extract().response();
+    }
+
+    private Response retrieveHearingSchedulesResponseForMissingAccessToken(final String api, final Map<String, Object> headersAsMap, final Map<String, String> paramsAsMap,final String basePath) {
+
+         return given()
+                 .queryParams(paramsAsMap)
+                .headers(headersAsMap)
+                .baseUri(basePath)
+                .basePath(api)
+                .when().get().then().extract().response();
+    }
+
+    private Response retrieveHearingSchedulesResponseForMissingOrInvalidHeader(final String api, final Map<String, Object> headersAsMap, final Map<String, String> paramsAsMap,final String basePath) {
+
+             return given()
+                     .auth()
+                     .oauth2(accessToken)
+                    .queryParams(paramsAsMap)
+                    .headers(headersAsMap)
+                    .baseUri(basePath)
+                    .basePath(api)
+                    .when().get().then().extract().response();
+
+    }
+
+}
---
+++ src/test/java/uk/gov/hmcts/futurehearings/hmi/unit/testing/util/HealthResponseVerifier.java
@@ -0,0 +1,176 @@
+package uk.gov.hmcts.futurehearings.hmi.unit.testing.util;
+
+import io.restassured.response.Response;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static uk.gov.hmcts.futurehearings.hmi.unit.testing.util.TestReporter.getObjStep;
+
+
+public class SchedulesResponseVerifier {
+
+    private static final String MISSING_SUB_KEY_ERROR = "Access denied due to missing subscription key. Make sure to include subscription key when making requests to an API.";
+    private static final String INVALID_SUB_KEY_ERROR = "Access denied due to invalid subscription key. Make sure to provide a valid key for an active subscription.";
+
+
+    public static void thenValidateResponseForInvalidResource(Response response) {
+        try {
+            Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
+            //assertEquals(2, responseMap.size());
+            assertEquals(404, response.getStatusCode(),"Status Code Validation:");
+            getObjStep().pass("Got the expected status code: 404");
+            assertEquals("Resource not found", responseMap.get(("message")),"Status Code Message Validation:");
+            getObjStep().pass("Got the expected message: " + responseMap.get(("message")));
+        } catch (AssertionError e) {
+            getObjStep().fail("Exception in " + e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            getObjStep().fail("Exception: " + e.getClass());
+            throw e;
+        }
+    }
+
+    public static void thenValidateResponseForRetrieve(Response response) {
+        try {
+
+            assertEquals(200, response.getStatusCode(),"Response Code Validation:");
+            getObjStep().pass("Got the expected response code: 200");
+
+        } catch (AssertionError e) {
+            getObjStep().fail("Exception in " + e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            getObjStep().fail("Exception: " + e.getClass());
+            throw e;
+        }
+    }
+
+    public static void  thenValidateResponseForCreate(Response response){
+        try{
+            assertEquals(201, response.getStatusCode(),"Response Code Validation:");
+            getObjStep().pass("Got the expected response code: 201");
+        }
+        catch (AssertionError e){
+            getObjStep().fail("Exception in "+e.getMessage());
+            throw e;
+        }
+        catch (Exception e){
+            getObjStep().fail("Exception: "+e.getClass());
+            throw e;
+        }
+    }
+
+    public static void thenValidateResponseForMissingSubscriptionKeyHeader(Response response) {
+
+        try {
+            Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
+            //assertEquals(2, responseMap.size());
+            assertEquals(401, response.getStatusCode(),"Status Code Validation:");
+            getObjStep().pass("Got the expected status code: 401");
+            assertEquals( MISSING_SUB_KEY_ERROR, responseMap.get(("message")),"Status Code Message Validation:");
+            getObjStep().pass("Got the expected message: " + responseMap.get(("message")));
+        } catch (AssertionError e) {
+            getObjStep().fail("Exception in " + e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            getObjStep().fail("Exception: " + e.getClass());
+            throw e;
+        }
+    }
+
+    public static void thenValidateResponseForInvalidSubscriptionKeyHeader(Response response) {
+
+        try {
+            Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
+            //assertEquals(2, responseMap.size());
+            assertEquals(401, response.getStatusCode(),"Status Code Validation:");
+            getObjStep().pass("Got the expected status code: 401");
+            assertEquals(INVALID_SUB_KEY_ERROR, responseMap.get(("message")),"Status Code Message Validation:");
+            getObjStep().pass("Got the expected message: " + responseMap.get(("message")));
+        } catch (AssertionError e) {
+            getObjStep().fail("Exception in " + e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            getObjStep().fail("Exception: " + e.getClass());
+            throw e;
+        }
+    }
+
+    public static void  thenValidateResponseForMissingOrInvalidAccessToken(Response response){
+
+        try{
+            Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
+            //assertEquals(2, responseMap.size());
+            assertEquals(401, response.getStatusCode(),"Status Code Validation:");
+            getObjStep().pass("Got the expected status code: 401");
+            assertEquals("Access denied due to invalid OAuth information", responseMap.get(("message")),"Status Code Message Validation:");
+            getObjStep().pass("Got the expected message: " + responseMap.get(("message")));
+        }
+        catch (AssertionError e){
+            getObjStep().fail("Exception in "+e.getMessage());
+            throw e;
+        }
+        catch (Exception e){
+            getObjStep().fail("Exception: "+e.getClass());
+            throw e;
+        }
+
+    }
+
+    public static void thenValidateResponseForMissingOrInvalidHeader(Response response, String missingField) {
+
+        try {
+            Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
+            //assertEquals(2, responseMap.size());
+            assertEquals(400, response.getStatusCode(),"Status Code Validation:");
+            getObjStep().pass("Got the expected status code: 400");
+            assertEquals("Missing/Invalid Header " + missingField, responseMap.get(("message")),"Status Code Message Validation:");
+            getObjStep().pass("Got the expected message: " + responseMap.get(("message")));
+        } catch (AssertionError e) {
+            getObjStep().fail("Exception in " + e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            getObjStep().fail("Exception: " + e.getClass());
+            throw e;
+        }
+
+    }
+
+    public static void thenValidateResponseForMissingOrInvalidAcceptHeader(Response response) {
+
+        try {
+            Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
+            //assertEquals(2, responseMap.size());
+            assertEquals(406, response.getStatusCode(),"Status Code Validation:");
+            getObjStep().pass("Got the expected status code: 406");
+            assertEquals("Missing/Invalid Media Type", responseMap.get(("message")),"Status Code Message Validation:");
+            getObjStep().pass("Got the expected message: " + responseMap.get(("message")));
+        } catch (AssertionError e) {
+            getObjStep().fail("Exception in " + e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            getObjStep().fail("Exception: " + e.getClass());
+            throw e;
+        }
+    }
+
+
+    public static void thenValidateResponseForMissingOrInvalidContentTypeHeader(Response response) {
+
+        try {
+            Map<String, String> responseMap = response.getBody().jsonPath().getMap("$");
+            //assertEquals(2, responseMap.size());
+            assertEquals(400, response.getStatusCode(),"Status Code Validation:");
+            getObjStep().pass("Got the expected status code: 400");
+            assertEquals("Missing/Invalid Media Type", responseMap.get(("message")),"Status Code Message Validation:");
+            getObjStep().pass("Got the expected message: " + responseMap.get(("message")));
+        } catch (AssertionError e) {
+            getObjStep().fail("Exception in " + e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            getObjStep().fail("Exception: " + e.getClass());
+            throw e;
+        }
+    }
+}
