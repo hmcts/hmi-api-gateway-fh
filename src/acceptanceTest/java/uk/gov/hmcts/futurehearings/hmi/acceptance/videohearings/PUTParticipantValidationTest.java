4ba2b8ec659a87c5004b9c95950e437607428d15
Matthew Summerbell <matt.summerbell@HMCTS.NET>
2021-10-28 09:09:13 +0100

    removed unused imports

--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/videohearings/PUTParticipantValidationTest.java
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/videohearings/PUTParticipantValidationTest.java
@@ -1,19 +1,14 @@
 package uk.gov.hmcts.futurehearings.hmi.acceptance.videohearings;
 
 import uk.gov.hmcts.futurehearings.hmi.Application;
-import uk.gov.hmcts.futurehearings.hmi.acceptance.common.delegate.CommonDelegate;
 import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.error.HMICommonErrorVerifier;
 import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.success.HMICommonSuccessVerifier;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectClasses;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
