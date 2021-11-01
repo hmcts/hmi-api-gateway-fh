e016f7c41a3d6136fcacf03ae4622b7855aa9a4e
Christopher Pateman <christopher.pateman@version1.com>
2021-11-01 12:19:56 +0000

    remove certs and add ca certs

--- infrastructure/terraform/28-api-management-custom-domain.tf
+++ infrastructure/terraform/28-api-management-custom-domain.tf
@@ -5,36 +5,17 @@ locals {
   cert_name     = replace(local.host_name, ".", "-")
 }
 
-data "azurerm_key_vault" "kv" {
-  name                = "acmedtssds${var.environment}"
-  resource_group_name = "sds-platform-${var.environment}-rg"
-}
-data "azurerm_key_vault_certificate" "cert" {
-  name         = local.cert_name
-  key_vault_id = data.azurerm_key_vault.kv.id
-}
 
 data "azurerm_api_management" "hmi_apim_svc" {
   name                = azurerm_api_management.hmi_apim.name
   resource_group_name = azurerm_resource_group.hmi_apim_rg.name
 }
-resource "azurerm_role_assignment" "kv_access" {
-  scope                = data.azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = data.azurerm_api_management.hmi_apim_svc.identity.0.principal_id
-}
-resource "azurerm_key_vault_access_policy" "policy" {
-  key_vault_id            = data.azurerm_key_vault.kv.id
-  tenant_id               = data.azurerm_client_config.current.tenant_id
-  object_id               = data.azurerm_api_management.hmi_apim_svc.identity.0.principal_id
-  key_permissions         = []
-  secret_permissions      = ["Get", "List"]
-  certificate_permissions = ["Get", "List"]
-  storage_permissions     = []
-}
 
-locals {
-  cert_url = replace(data.azurerm_key_vault_certificate.cert.secret_id, "/${data.azurerm_key_vault_certificate.cert.version}", "")
+module "cert" {
+  source        = "git::https://github.com/hmcts/terraform-module-certificate.git?ref=add-url"
+  environment   = var.environment
+  domain_prefix = "hmi-apim"
+  object_id     = data.azurerm_api_management.hmi_apim_svc.identity.0.principal_id
 }
 
 resource "azurerm_api_management_custom_domain" "custom_domain" {
@@ -42,13 +23,12 @@ resource "azurerm_api_management_custom_domain" "custom_domain" {
 
   proxy {
     host_name                    = local.host_name
-    key_vault_id                 = local.cert_url
+    key_vault_id                 = module.cert.url
     negotiate_client_certificate = true
     default_ssl_binding          = true
   }
 
   depends_on = [
-    data.azurerm_key_vault_certificate.cert,
-    azurerm_role_assignment.kv_access
+    module.cert
   ]
 }
