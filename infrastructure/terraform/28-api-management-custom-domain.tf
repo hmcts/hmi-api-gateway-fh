3806ec756d37c2a5f5238eccde7d91474bf1ce3b
Christopher Pateman <christopher.pateman@version1.com>
2022-01-05 10:58:20 +0000

    add user identity

--- infrastructure/terraform/28-api-management-custom-domain.tf
+++ infrastructure/terraform/28-api-management-custom-domain.tf
@@ -9,19 +9,8 @@ module "cert" {
   source        = "git::https://github.com/hmcts/terraform-module-certificate.git?ref=master"
   environment   = var.environment
   domain_prefix = "hmi-apim"
-  #object_id     = azurerm_api_management.hmi_apim.identity.0.principal_id
+  object_id     = data.azurerm_user_assigned_identity.mi.principal_id
 }
-data "azurerm_key_vault" "acmekv" {
-  name                = "acmedtssds${var.environment}"
-  resource_group_name = "sds-platform-${var.environment}-rg"
-}
-resource "azurerm_role_assignment" "kv_access" {
-  scope                = data.azurerm_key_vault.acmekv.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_api_management.hmi_apim.identity.0.principal_id
-}
-
-
 
 resource "azurerm_api_management_custom_domain" "custom_domain" {
   api_management_id = azurerm_api_management.hmi_apim.id
