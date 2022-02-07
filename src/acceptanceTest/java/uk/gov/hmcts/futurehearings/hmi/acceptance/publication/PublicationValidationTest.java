3fa02e5b46acbbc7ed67589f452107b7e4cd0e48
ShufenZ <shufen.zhang@version1.com>
2022-02-07 12:24:59 +0000

    HMIS-993 tidy up imports

--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/publication/PublicationValidationTest.java
+++ src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/publication/PublicationValidationTest.java
@@ -1,19 +1,21 @@
 package uk.gov.hmcts.futurehearings.hmi.acceptance.publication;
 
 import io.restassured.RestAssured;
-        import lombok.extern.slf4j.Slf4j;
-        import org.junit.jupiter.api.BeforeAll;
-        import org.springframework.beans.factory.annotation.Value;
-        import org.springframework.boot.test.context.SpringBootTest;
-        import org.springframework.http.HttpStatus;
-        import org.springframework.test.context.ActiveProfiles;
-        import uk.gov.hmcts.futurehearings.hmi.Application;
-        import uk.gov.hmcts.futurehearings.hmi.acceptance.common.test.HMICommonHeaderTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.futurehearings.hmi.Application;
+import uk.gov.hmcts.futurehearings.hmi.acceptance.common.test.HMICommonHeaderTest;
 
-        import static io.restassured.config.EncoderConfig.encoderConfig;
-        import static uk.gov.hmcts.futurehearings.hmi.acceptance.common.security.OAuthTokenGenerator.generateOAuthToken;
+import static io.restassured.config.EncoderConfig.encoderConfig;
+import static uk.gov.hmcts.futurehearings.hmi.acceptance.common.security.OAuthTokenGenerator.generateOAuthToken;
 
 @Slf4j
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(classes = {Application.class})
 @ActiveProfiles("acceptance")
 public abstract class PublicationValidationTest extends HMICommonHeaderTest {
