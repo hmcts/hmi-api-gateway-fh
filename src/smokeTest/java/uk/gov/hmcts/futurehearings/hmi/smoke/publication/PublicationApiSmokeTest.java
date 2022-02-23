b515c15d67950be5794c60f8012a0a8142e948fd
ShufenZ <shufen.zhang@version1.com>
2022-02-23 17:58:48 +0000

    HMIS-1005 Added smoke test for P&I API health check endpoint

---
+++ src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/publication/PublicationApiSmokeTest.java
@@ -0,0 +1,42 @@
+package uk.gov.hmcts.futurehearings.hmi.smoke.publication;
+
+import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.futurehearings.hmi.Application;
+import uk.gov.hmcts.futurehearings.hmi.smoke.common.rest.RestClient;
+import uk.gov.hmcts.futurehearings.hmi.smoke.common.test.SmokeTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
+@SpringBootTest(classes = {Application.class})
+@ActiveProfiles("smoke")
+@DisplayName("Smoke Test for the HMI P&I API Health Check endpoint")
+@SuppressWarnings("java:S2187")
+class PublicationApiSmokeTest extends SmokeTest {
+
+    @Value("${publicationHealthCheckRootContext}")
+    private String publicationHealthCheckRootContext;
+
+    @BeforeAll
+    public void initialiseValues() throws Exception {
+        super.initialiseValues();
+        setRootContext(publicationHealthCheckRootContext);
+    }
+
+    @Test
+    @DisplayName("Smoke Test to test the listings endpoint")
+    public void testPublicationHmiApiGet() {
+        Response response = RestClient.makeGetRequest(getHeadersAsMap(), getAuthorizationToken(),
+                getRootContext());
+
+        assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+    }
+}
