643826e9f0710b2b1c5fde2679024519315ea28d
Christopher Pateman <christopher.pateman@version1.com>
2022-05-10 16:53:54 +0100

    Add KV Access

--- infrastructure/terraform/25-api-management-subscription.tf
+++ infrastructure/terraform/25-api-management-subscription.tf
@@ -18,6 +18,14 @@ resource "azurerm_api_management_subscription" "hmi_apim_subscription" {
   allow_tracing       = var.environment == "sbox" || var.environment == "dev" || var.environment == "test" ? true : false
 }
 
+
+resource "azurerm_key_vault_access_policy" "shared_kv_premissions" {
+  key_vault_id       = data.azurerm_key_vault.infra_key_vault.id
+  tenant_id          = data.azurerm_key_vault.infra_key_vault.tenant_id
+  object_id          = azurerm_api_management.hmi_apim.identity[0].principal_id
+  secret_permissions = var.secret_permissions
+}
+
 resource "azurerm_key_vault_secret" "subscription_key" {
   name         = "${var.prefix}-${var.product}-sub-key"
   value        = azurerm_api_management_subscription.hmi_apim_subscription.primary_key
