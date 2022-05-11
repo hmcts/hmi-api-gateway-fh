43feec771b354e2c13fbd429c15839da005509d0
mjotichmcts <70706159+mjotichmcts@users.noreply.github.com>
2022-05-11 17:41:37 +0100

    Hmis 1032 dev (#533)
    * added cft auth
    
    * using quotes for cft var
    
    * moved cache update
    
    * HMI payload updated
    
    * req body to 500 and headers
    
    * HMIS-1022 Added acceptance and functional tests to McGirr API spec v1.11.8 for those new URL parameters which can be allocated with valid values.
    
    * updated payload
    
    * HMIS-1017 Added acceptance tests to test URL parameters for the new API endpoint GET Reservation which is added in McGirr API spec v1.11.8 and implemented in v1.11.8 release.
    
    * initial version
    
    * added cpp la deployment
    
    * added tags
    
    * updated tag name for businessArea
    
    * removed unused vars
    
    * updated dependsOn
    
    * updated tags
    
    * tags
    
    * removed loging of tags
    
    * updated LA  app name
    
    * updated LA core name
    
    * fixed name
    
    * added hearing  check
    
    * sacred deployment
    
    * fixed workflow name
    
    * added eventGrid
    
    * added nkeyvault connection
    
    * added missing env
    
    * HMIS-1039: Adding logic to create connectivity with CFT
    
    * HMIS-1022 Added functional tests for URL params for GET Sessions and enabled disabled test on receipt of information from McGirr. Added overload method in SessionsLookUpSteps for testing speficied Status codes in addition to 200.
    
    * HMIS-1039: Adding logic to create connectivity with CFT
    
    * removed sacred LA
    
    * updated Snow and HMI calls
    
    * introduced params
    
    * added keyvault connector
    
    * added  KV connection
    
    * updated params
    
    * added delete csv
    
    * HMIS-1001: Adding logic to create connectivity with CFT
    
    * HMIS-1031: Adding logic to create connectivity with CFT
    
    * HMIS-1022 Added functional tests for more URL params for GET Sessions with one test disabled due to bug and needs enabled when it is fixed by McGirr.
    
    * HMIS-1022 Added acceptance tests for more URL params for GET Sessions
    
    * removed unused params
    
    * Update resource tags
    
    * add built from tags
    
    Co-authored-by: Milan Jotic <milan.jotic@beo-tech.com>
    Co-authored-by: ShufenZ <shufen.zhang@version1.com>
    Co-authored-by: Shufen Zhang <85679190+ShufenZ@users.noreply.github.com>
    Co-authored-by: 463952 <subroto.mukherjee@HMCTS.NET>
    Co-authored-by: Subroto Mukherjee <53174127+subrotomukherjee@users.noreply.github.com>
    Co-authored-by: Adam Todd <adam.todd@hmcts.net>
    Co-authored-by: adamtodd0 <94545229+adamtodd0@users.noreply.github.com>

--- src/functionalTest/java/uk/gov/hmcts/futurehearings/hmi/functional/sessions/SessionsLookUpTest.java
+++ src/functionalTest/java/uk/gov/hmcts/futurehearings/hmi/functional/sessions/SessionsLookUpTest.java
@@ -5,12 +5,12 @@ import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import net.thucydides.core.annotations.Narrative;
 import net.thucydides.core.annotations.Steps;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.jupiter.api.Disabled;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.futurehearings.hmi.Application;
 import uk.gov.hmcts.futurehearings.hmi.functional.common.test.FunctionalTest;
@@ -40,7 +40,7 @@ public class SessionsLookUpTest extends FunctionalTest {
     }
 
     @Test
-    public void testSuccessfulGetSessionForSessionRequestType() {
+    public void testSuccessfulGetSessionForRequestSessionType() {
         Map<String, String> queryParameters = new HashMap<>();
         queryParameters.put("requestSessionType", "CJ");
         headersAsMap.put("Destination-System", "SNL");
@@ -51,7 +51,7 @@ public class SessionsLookUpTest extends FunctionalTest {
     }
 
     @Test
-    public void testSuccessfulGetSessionForSessionRequestTypeAndRequestDuration() {
+    public void testSuccessfulGetSessionForRequestSessionTypeAndDuration() {
         Map<String, String> queryParameters = new HashMap<>();
         queryParameters.put("requestSessionType", "DS");
         queryParameters.put("requestDuration", "360");
@@ -63,7 +63,7 @@ public class SessionsLookUpTest extends FunctionalTest {
     }
 
     @Test
-    public void testSuccessfulGetSessionForSessionRequestTypeAndRequestLocation() {
+    public void testSuccessfulGetSessionForRequestSessionTypeAndLocation() {
         Map<String, String> queryParameters = new HashMap<>();
         queryParameters.put("requestSessionType", "DS");
         queryParameters.put("requestLocationId", "300");
@@ -77,10 +77,11 @@ public class SessionsLookUpTest extends FunctionalTest {
     }
 
     @Test
-    public void testSuccessfulGetSessionForSessionRequestTypeAndRequestJudgeType() {
+    public void testSuccessfulGetSessionForRequestSessionTypeAndStartDateAndEndDate() {
         Map<String, String> queryParameters = new HashMap<>();
         queryParameters.put("requestSessionType", "CJ");
-        queryParameters.put("requestJudgeType", "PUBLAW");
+        queryParameters.put("requestStartDate", "2022-02-25T09:00:00Z");
+        queryParameters.put("requestEndDate", "2022-03-01T09:00:00Z");
         headersAsMap.put("Destination-System", "SNL");
         sessionsLookUpSteps.checkSessionsForAllTheRelevantQueryParameters(sessionsRootContext,
                 headersAsMap,
@@ -89,11 +90,153 @@ public class SessionsLookUpTest extends FunctionalTest {
     }
 
     @Test
-    public void testSuccessfulGetSessionForSessionRequestTypeAndRequestStartDateAndRequestEndDate() {
+    public void testSuccessfulGetSessionForRequestSessionTypeAndPanelType() {
         Map<String, String> queryParameters = new HashMap<>();
         queryParameters.put("requestSessionType", "CJ");
-        queryParameters.put("requestStartDate", "2022-02-25T09:00:00Z");
-        queryParameters.put("requestEndDate", "2022-03-01T09:00:00Z");
+        queryParameters.put("requestPanelType", "Adult");
+        headersAsMap.put("Destination-System", "SNL");
+        sessionsLookUpSteps.checkSessionsForAllTheRelevantQueryParameters(sessionsRootContext,
+                headersAsMap,
+                authorizationToken,
+                queryParameters);
+    }
+
+    @Test
+    public void testSuccessfulGetSessionForRequestSessionTypeAndJurisdiction() {
+        Map<String, String> queryParameters = new HashMap<>();
+        queryParameters.put("requestSessionType", "CJ");
+        queryParameters.put("requestJurisdiction", "CIV");
+        headersAsMap.put("Destination-System", "SNL");
+        sessionsLookUpSteps.checkSessionsForAllTheRelevantQueryParameters(sessionsRootContext,
+                headersAsMap,
+                authorizationToken,
+                queryParameters);
+    }
+
+    @Test
+    public void testSuccessfulGetSessionForRequestSessionTypeAndGroupBooking() {
+        Map<String, String> queryParameters = new HashMap<>();
+        queryParameters.put("requestSessionType", "CJ");
+        queryParameters.put("requestGroupBooking", "false");
+        headersAsMap.put("Destination-System", "SNL");
+        sessionsLookUpSteps.checkSessionsForAllTheRelevantQueryParameters(sessionsRootContext,
+                headersAsMap,
+                authorizationToken,
+                queryParameters);
+    }
+
+    @Test
+    public void testSuccessfulGetSessionForRequestSessionTypeAndAvailableDuration() {
+        Map<String, String> queryParameters = new HashMap<>();
+        queryParameters.put("requestSessionType", "CJ");
+        queryParameters.put("requestAvailableDuration", "200");
+        headersAsMap.put("Destination-System", "SNL");
+        sessionsLookUpSteps.checkSessionsForAllTheRelevantQueryParameters(sessionsRootContext,
+                headersAsMap,
+                authorizationToken,
+                queryParameters);
+    }
+
+    @Test
+    public void testSuccessfulGetSessionForRequestSessionTypeAndAvailableSlotCount() {
+        Map<String, String> queryParameters = new HashMap<>();
+        queryParameters.put("requestSessionType", "CJ");
+        queryParameters.put("requestAvailableSlotCount", "2");
+        headersAsMap.put("Destination-System", "SNL");
+        sessionsLookUpSteps.checkSessionsForAllTheRelevantQueryParameters(sessionsRootContext,
+                headersAsMap,
+                authorizationToken,
+                queryParameters);
+    }
+
+    @Test
+    public void testSuccessfulGetSessionForRequestSessionTypeAndReturnAllSessions() {
+        Map<String, String> queryParameters = new HashMap<>();
+        queryParameters.put("requestSessionType", "CJ");
+        queryParameters.put("requestReturnAllSessions", "false");
+        headersAsMap.put("Destination-System", "SNL");
+        sessionsLookUpSteps.checkSessionsForAllTheRelevantQueryParameters(sessionsRootContext,
+                headersAsMap,
+                authorizationToken,
+                queryParameters);
+    }
+
+    @Test
+    public void testSuccessfulGetSessionForRequestSessionTypeAndIncludeDummyRooms() {
+        Map<String, String> queryParameters = new HashMap<>();
+        queryParameters.put("requestSessionType", "CJ");
+        queryParameters.put("requestIncludeDummyRooms", "false");
+        headersAsMap.put("Destination-System", "SNL");
+        sessionsLookUpSteps.checkSessionsForAllTheRelevantQueryParameters(sessionsRootContext,
+                headersAsMap,
+                authorizationToken,
+                queryParameters);
+    }
+
+    @Test
+    public void testInvalidGetSessionForSessionRequestTypeAndAvailableSlotCountMinValueMinusOne() {
+        Map<String, String> queryParameters = new HashMap<>();
+        queryParameters.put("requestSessionType", "CJ");
+        queryParameters.put("requestAvailableSlotCount", "0");
+        headersAsMap.put("Destination-System", "SNL");
+        sessionsLookUpSteps.checkSessionsForAllTheRelevantQueryParameters(sessionsRootContext,
+                headersAsMap,
+                authorizationToken,
+                queryParameters,
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    public void testSuccessfulGetSessionForSessionRequestTypeAndAvailableSlotCountMinValue() {
+        Map<String, String> queryParameters = new HashMap<>();
+        queryParameters.put("requestSessionType", "CJ");
+        queryParameters.put("requestAvailableSlotCount", "1");
+        headersAsMap.put("Destination-System", "SNL");
+        sessionsLookUpSteps.checkSessionsForAllTheRelevantQueryParameters(sessionsRootContext,
+                headersAsMap,
+                authorizationToken,
+                queryParameters);
+    }
+
+    @Test
+    public void testSuccessfulGetSessionForSessionRequestTypeAndAvailableSlotCountMinValuePlusOne() {
+        Map<String, String> queryParameters = new HashMap<>();
+        queryParameters.put("requestSessionType", "CJ");
+        queryParameters.put("requestAvailableSlotCount", "2");
+        headersAsMap.put("Destination-System", "SNL");
+        sessionsLookUpSteps.checkSessionsForAllTheRelevantQueryParameters(sessionsRootContext,
+                headersAsMap,
+                authorizationToken,
+                queryParameters);
+    }
+
+    @Test
+    public void testSuccessfulGetSessionForRequestServiceCode() {
+        Map<String, String> queryParameters = new HashMap<>();
+        queryParameters.put("requestServiceCode", "1");
+        headersAsMap.put("Destination-System", "SNL");
+        sessionsLookUpSteps.checkSessionsForAllTheRelevantQueryParameters(sessionsRootContext,
+                headersAsMap,
+                authorizationToken,
+                queryParameters);
+    }
+
+    @Test
+    public void testSuccessfulGetSessionForRequestHearingType() {
+        Map<String, String> queryParameters = new HashMap<>();
+        queryParameters.put("requestHearingType", "21");
+        headersAsMap.put("Destination-System", "SNL");
+        sessionsLookUpSteps.checkSessionsForAllTheRelevantQueryParameters(sessionsRootContext,
+                headersAsMap,
+                authorizationToken,
+                queryParameters);
+    }
+
+    @Disabled("Test fails as requestRoomAttributes does not accept any valid values. Waiting for McGirr to fix this.")
+    // @Test
+    public void testSuccessfulGetSessionForRequestRoomAttributes() {
+        Map<String, String> queryParameters = new HashMap<>();
+        queryParameters.put("requestRoomAttributes", "8");
         headersAsMap.put("Destination-System", "SNL");
         sessionsLookUpSteps.checkSessionsForAllTheRelevantQueryParameters(sessionsRootContext,
                 headersAsMap,
