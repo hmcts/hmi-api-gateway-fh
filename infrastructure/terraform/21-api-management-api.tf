158a591150a8edb7561fc6b5a5411634aca49703
Milan Jotic <milan.jotic@beo-tech.com>
2022-05-31 10:33:46 +0100

    added service url

--- infrastructure/terraform/21-api-management-api.tf
+++ infrastructure/terraform/21-api-management-api.tf
@@ -7,6 +7,8 @@ resource "azurerm_api_management_api" "hmi_apim_api" {
   path                = var.prefix
   protocols           = var.protocols
 
+  service_url         = var.service_url
+
   subscription_required = false
 
   import {
