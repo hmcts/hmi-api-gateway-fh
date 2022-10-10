14916a9f20e6f4fcbd7cd036a4fe0afe2c5184ac
Manoj Ravipati <Manoj.Ravipati@HMCTS.NET>
2022-10-10 13:43:45 +0100

    Updated Elink health  policy

--- src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/people/PeopleApiSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/people/PeopleApiSmokeTest.java
@@ -19,7 +19,6 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
-//@Disabled("Failing due to issue, to be re-enabled in future ticket once fix implemented")
 @SpringBootTest(classes = {Application.class})
 @ActiveProfiles("smoke")
 @DisplayName("Smoke Test for the HMI People Context")
@@ -29,7 +28,6 @@ class PeopleApiSmokeTest extends SmokeTest {
     @Value("${peopleHealthcheck}")
     private String peopleHealthcheck;
 
-    @BeforeAll
     public void initialiseValues() throws Exception {
         this.setDestinationSystem("ELINKS");
         super.initialiseValues();
@@ -37,7 +35,6 @@ class PeopleApiSmokeTest extends SmokeTest {
     }
 
     @Test
-   // @DisplayName("Smoke Test to test the people endpoint")
     void testPeopleHmiApiGet() {
         Response response = RestClient.makeGetRequest(getHeadersAsMap(),
                 getAuthorizationToken(),
