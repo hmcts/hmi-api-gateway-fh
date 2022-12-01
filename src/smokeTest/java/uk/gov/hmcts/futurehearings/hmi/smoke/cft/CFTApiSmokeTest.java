a09f5e1cc60935375e790d7d9bdd64bba25b5f3a
praveenrach <praveen.rachapally@HMCTS.NET>
2022-12-01 16:08:37 +0000

    added code for cft health check, updated the cftHost related urls

---
+++ src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/cft/CFTApiSmokeTest.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.futurehearings.hmi.smoke.cft;
+
+import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.futurehearings.hmi.Application;
+import uk.gov.hmcts.futurehearings.hmi.smoke.common.rest.RestClient;
+import uk.gov.hmcts.futurehearings.hmi.smoke.common.test.SmokeTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
+@SpringBootTest(classes = {Application.class})
+@ActiveProfiles("smoke")
+@Disabled
+@DisplayName("Smoke Test for the HMI CFT Health Check Context")
+@SuppressWarnings("java:S2187")
+public class CFTApiSmokeTest extends SmokeTest {
+
+    @Value("${cftHealthCheckRootContext}")
+    private String cftHealthCheckRootContext;
+
+    @BeforeAll
+    public void initialiseValues() throws Exception {
+        this.setDestinationSystem("CFT");
+        super.initialiseValues();
+        setRootContext(cftHealthCheckRootContext);
+    }
+
+    @Test
+    @DisplayName("Smoke Test to test the hearings endpoint")
+    void testHearingHmiApiGet() {
+        Response response = RestClient.makeGetRequest(getHeadersAsMap(), getAuthorizationToken(),
+                getRootContext());
+
+        assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+    }
+}
