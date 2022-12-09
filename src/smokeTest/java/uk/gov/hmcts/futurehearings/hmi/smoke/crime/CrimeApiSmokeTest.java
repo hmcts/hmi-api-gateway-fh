f6db71162cc48c77a5b6fdf2906be2fc17d12f1b
praveenrach <praveen.rachapally@HMCTS.NET>
2022-12-09 11:02:02 +0000

    added headers for snl health check, disabled crime smoke tests

--- src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/crime/CrimeApiSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/futurehearings/hmi/smoke/crime/CrimeApiSmokeTest.java
@@ -20,6 +20,7 @@ import org.springframework.test.context.ActiveProfiles;
 @Slf4j
 @SpringBootTest(classes = {Application.class})
 @ActiveProfiles("smoke")
+@Disabled
 @DisplayName("Smoke Test for the HMI Hearing Context")
 @SuppressWarnings("java:S2187")
 public class CrimeApiSmokeTest extends SmokeTest {
