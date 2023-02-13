8887c4dd446c298800b350e98720310edd2d3d95
Adam Todd <adam.todd1@hmcts.net>
2023-02-13 13:43:01 +0000

    Disable rota LAs in stg

--- src/acceptanceTest/java/uk/gov/hmcts/futurehearings/hmi/acceptance/common/verify/error/HMIErrorVerifier.java
+++
@@ -1,6 +0,0 @@
-package uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.error;
-
-import uk.gov.hmcts.futurehearings.hmi.acceptance.common.verify.HMIVerifier;
-
-public interface HMIErrorVerifier extends HMIVerifier {
-}
